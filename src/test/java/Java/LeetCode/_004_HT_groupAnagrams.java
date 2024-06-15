ccb598f2ed0e2cb4e253d7c225df1b1211778d7b
avdhutssh <avdhut.ssh@gmail.com>
2024-06-15 22:30:22 +0530

    HT: group the anagrams

---
+++ src/test/java/Java/LeetCode/_004_HT_groupAnagrams.java
@@ -0,0 +1,36 @@
+package Java.LeetCode;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.List;
+
+public class _004_HT_groupAnagrams {
+
+	public static void main(String[] args) {
+	
+        System.out.println(_01_Using_Hashmap(new String[]{"eat", "tea", "tan", "ate", "nat", "bat"})); //[[eat, tea, ate], [tan, nat], [bat]]
+        System.out.println(_01_Using_Hashmap(new String[]{"abc", "cba", "bac", "foo", "bar"})); //[[abc, cba, bac], [foo], [bar]]
+        System.out.println(_01_Using_Hashmap(new String[]{"listen", "silent", "triangle", "integral", "garden", "ranged"})); //[[listen, silent], [triangle, integral], [garden, ranged]]
+
+	}
+
+	private static List<List<String>> _01_Using_Hashmap(String[] strings) {
+		HashMap<String, List<String>> anagramGroup = new HashMap<String, List<String>>();
+		for (String str : strings) {
+			char[] chars = str.toCharArray();
+			Arrays.sort(chars);
+			String sortedStr = new String(chars);
+			if(anagramGroup.containsKey(sortedStr)) {
+				anagramGroup.get(sortedStr).add(str);
+			}else {
+				List<String> group = new ArrayList<String>();
+				group.add(str);
+				anagramGroup.put(sortedStr, group);
+			}
+		}	
+		return new ArrayList<List<String>>(anagramGroup.values());
+	}
+
+}
