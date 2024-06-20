e0f1d5b96da10bb19a389c81e697763f4ac4b651
avdhutssh <avdhut.ssh@gmail.com>
2024-06-20 12:16:28 +0530

    find peak element index By_DivideAndConquer_Iteratively

--- src/test/java/Java/UdemyFifthyCodingChallenges/_10_findPeakIndex.java
+++ src/test/java/Java/UdemyFifthyCodingChallenges/_10_findPeakIndex.java
@@ -11,9 +11,16 @@ public class _10_findPeakIndex {
 		System.out.println(_01_ByTraversingWholeArray(arr2));
 		System.out.println(_01_ByTraversingWholeArray(arr3));
 		System.out.println("-----------------------------------");
+		
+		System.out.println(_02_By_DivideAndConquer_Iteratively(arr1));
+		System.out.println(_02_By_DivideAndConquer_Iteratively(arr2));
+		System.out.println(_02_By_DivideAndConquer_Iteratively(arr3));
+		System.out.println("-----------------------------------");
 	}
 
 	private static int _01_ByTraversingWholeArray(int[] arr) {
+		// Time complexity: O(n)
+		// Space complexity: O(1)
 		for (int i = 0; i < arr.length; i++) {
 			if ((i == 0 || arr[i] >= arr[i - 1]) && (i == arr.length - 1 || arr[i] >= arr[i + 1]))
 				return i;
@@ -22,4 +29,18 @@ public class _10_findPeakIndex {
 		return 0;
 	}
 
+	private static int _02_By_DivideAndConquer_Iteratively(int[] arr) {
+		// Time complexity: O(nlogn)
+		// Space complexity: O(1)
+		int left = 0;
+		int right = arr.length - 1;
+		while (left < right) {
+			int mid = (left + right) / 2;
+			if (arr[mid] < arr[mid + 1])
+				left = mid + 1;
+			else
+				right = mid;
+		}
+		return left;
+	}
 }
