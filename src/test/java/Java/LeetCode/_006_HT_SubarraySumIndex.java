3d36fb0d751ab6b2173ad917e758cc87027c7efe
avdhutssh <avdhut.ssh@gmail.com>
2024-06-21 17:37:54 +0530

    _006_HT_SubarraySumIndex

---
+++ src/test/java/Java/LeetCode/_006_HT_SubarraySumIndex.java
@@ -0,0 +1,60 @@
+package Java.LeetCode;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+public class _006_HT_SubarraySumIndex {
+
+	public static void main(String[] args) {
+		int[] nums1 = { 1, 2, 3, 4, 5 }, nums2 = { -1, 2, 3, -4, 5 }, nums3 = { 2, 3, 4, 5, 6 }, nums4 = {};
+		int target1 = 9, target2 = 0, target3 = 3, target4 = 0;
+		Arrays.stream(_01_BruteForceApproach(nums1, target1)).forEach(value -> System.out.print(value + " ")); // [1, 3]
+		System.out.println();
+		Arrays.stream(_01_BruteForceApproach(nums2, target2)).forEach(value -> System.out.print(value + " ")); // [0, 3]
+		System.out.println();
+		Arrays.stream(_01_BruteForceApproach(nums3, target3)).forEach(value -> System.out.print(value + " ")); // [1, 1]
+		System.out.println();
+		Arrays.stream(_01_BruteForceApproach(nums4, target4)).forEach(value -> System.out.print(value + " ")); // []
+		System.out.println("-----------------------------------------------------");
+		
+		Arrays.stream(_02_Using_HashMap(nums1, target1)).forEach(value -> System.out.print(value + " ")); // [1, 3]
+		System.out.println();
+		Arrays.stream(_02_Using_HashMap(nums2, target2)).forEach(value -> System.out.print(value + " ")); // [0, 3]
+		System.out.println();
+		Arrays.stream(_02_Using_HashMap(nums3, target3)).forEach(value -> System.out.print(value + " ")); // [1, 1]
+		System.out.println();
+		Arrays.stream(_02_Using_HashMap(nums4, target4)).forEach(value -> System.out.print(value + " ")); // []
+	}
+
+	private static int[] _01_BruteForceApproach(int[] nums, int target) {
+		// Time complexity: O(n^2)
+		// Space complexity: O(1)
+		for (int i = 0; i < nums.length; i++) {
+			int sum = 0;
+			for (int j = i; j < nums.length; j++) {
+				sum += nums[j];
+				if (sum == target)
+					return new int[] { i, j };
+			}
+		}
+		return new int[] {};
+	}
+
+	static int[] _02_Using_HashMap(int[] arr, int target) {
+		// Time complexity: O(n)
+		// Space complexity: O(n)
+		Map<Integer, Integer> hm = new HashMap<Integer, Integer>();
+		int sum = 0;
+		hm.put(0, -1);
+		for (int i = 0; i < arr.length; i++) {
+			sum += arr[i];
+			if (hm.containsKey(sum - target)) {
+				return new int[] { hm.get(sum - target) + 1, i };
+			}
+			hm.put(sum, i);
+		}
+		return new int[] {};
+
+	}
+}
--- src/test/java/Java/UdemyFifthyCodingChallenges/_13_substrIndex.java
+++ src/test/java/Java/UdemyFifthyCodingChallenges/_13_substrIndex.java
@@ -3,30 +3,31 @@ package Java.UdemyFifthyCodingChallenges;
 public class _13_substrIndex {
 
 	public static void main(String[] args) {
-		String str1 = "inside", str2 = "side", str3="in", str4="code";
-		
-		System.out.println(_01_BruteForceApproach(str1,str2));  //2
-		System.out.println(_01_BruteForceApproach(str1,str3));  //0
-		System.out.println(_01_BruteForceApproach(str1,str4));  //-1
+		String str1 = "inside", str2 = "side", str3 = "in", str4 = "code";
+
+		System.out.println(_01_BruteForceApproach(str1, str2)); // 2
+		System.out.println(_01_BruteForceApproach(str1, str3)); // 0
+		System.out.println(_01_BruteForceApproach(str1, str4)); // -1
 
 	}
 
-    public static int _01_BruteForceApproach(String str1, String str2){
-    	// Time complexity: O(nm)
-    	// Space complexity: O(1)
-        int n = str1.length();
-        int m = str2.length();
-        for(int i =0; i<=n-m; i++){
-            boolean found = true;
-            for(int j=0; j<m; j++){
-                if(str1.charAt(i+j) != str2.charAt(j)){
-                    found = false;
-                    break;
-                }
-            }
-            if(found) return i;
-        }
-        return -1;
-    }
-    
+	public static int _01_BruteForceApproach(String str1, String str2) {
+		// Time complexity: O(nm)
+		// Space complexity: O(1)
+		int n = str1.length();
+		int m = str2.length();
+		for (int i = 0; i <= n - m; i++) {
+			boolean found = true;
+			for (int j = 0; j < m; j++) {
+				if (str1.charAt(i + j) != str2.charAt(j)) {
+					found = false;
+					break;
+				}
+			}
+			if (found)
+				return i;
+		}
+		return -1;
+	}
+
 }
