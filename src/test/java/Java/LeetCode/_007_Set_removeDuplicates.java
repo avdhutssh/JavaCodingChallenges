7d9b93cf78bec062d4dd409d3d7bab3248459c4b
avdhutssh <avdhut.ssh@gmail.com>
2024-06-22 15:21:46 +0530

    _007_Set_removeDuplicates using set based filtering by direct conversion

--- src/test/java/Java/LeetCode/_007_Set_removeDuplicates.java
+++ src/test/java/Java/LeetCode/_007_Set_removeDuplicates.java
@@ -10,6 +10,7 @@ public class _007_Set_removeDuplicates {
 	public static void main(String[] args) {
 		List<Integer> myList = List.of(1, 2, 3, 4, 1, 2, 5, 6, 7, 3, 4, 8, 9, 5); // [1, 2, 3, 4, 5, 6, 7, 8, 9]
 		System.out.println(_01_Using_SetBasedFiltering(myList));
+		System.out.println(_02_Using_SetBasedFilteringWithDirectConversion(myList));
 	}
 
 	static List<Integer> _01_Using_SetBasedFiltering(List<Integer> elements) {
@@ -21,5 +22,12 @@ public class _007_Set_removeDuplicates {
 		}
 		List<Integer> unique = new ArrayList<Integer>(hs);
 		return unique;
+	}
+
+	static List<Integer> _02_Using_SetBasedFilteringWithDirectConversion(List<Integer> elements) {
+		// Time complexity: O(n)
+		// Space complexity: O(n)
+		Set<Integer> hs = new HashSet<Integer>(elements);
+		return new ArrayList<Integer>(hs);
 	}
 }
