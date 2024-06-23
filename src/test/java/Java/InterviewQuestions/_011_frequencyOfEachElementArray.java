b2ca34cc35639c0a8e890b022513b535bae07546
avdhutssh <avdhut.ssh@gmail.com>
2024-06-23 17:16:53 +0530

    frequency of each element in the array using hashmap

--- src/test/java/Java/InterviewQuestions/_011_frequencyOfEachElementArray.java
+++ src/test/java/Java/InterviewQuestions/_011_frequencyOfEachElementArray.java
@@ -1,11 +1,15 @@
 package Java.InterviewQuestions;
 
+import java.util.HashMap;
+
 public class _011_frequencyOfEachElementArray {
 
 	public static void main(String[] args) {
-		int[] arr1 = { 10, 20, 20, 10, 10, 20, 5, 20 }; // 10 3 20 4 5 1
+		int[] arr = { 10, 20, 20, 10, 10, 20, 5, 20 }; // 10 3 20 4 5 1
 
-		_01_Using_BruteForceApproach(arr1);
+		_01_Using_BruteForceApproach(arr);
+		System.out.println("------------------------");
+		System.out.println(_02_Using_HashMap(arr));
 	}
 
 	private static void _01_Using_BruteForceApproach(int[] arr) {
@@ -17,7 +21,7 @@ public class _011_frequencyOfEachElementArray {
 				int freq = 1;
 				for (int j = i + 1; j < arr.length; j++) {
 					if (arr[i] == arr[j]) {
-						visited[j] =true;
+						visited[j] = true;
 						freq++;
 					}
 				}
@@ -26,4 +30,13 @@ public class _011_frequencyOfEachElementArray {
 		}
 	}
 
+	private static HashMap<Integer, Integer> _02_Using_HashMap(int[] arr) {
+		// Time complexity: O(n)
+		// Space complexity: O(n)
+		HashMap<Integer, Integer> freqMap = new HashMap<Integer, Integer>();
+		for (int num : arr) {
+			freqMap.put(num, freqMap.getOrDefault(num, 0) + 1);
+		}
+		return freqMap;
+	}
 }
