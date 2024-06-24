a03b1fd0a499c4e73bbe08d1d1afb82c74b8640e
avdhutssh <avdhut.ssh@gmail.com>
2024-06-24 20:39:45 +0530

    sum of all elements in java using while loop

--- src/test/java/Java/InterviewQuestions/_017_SumItemsInArray.java
+++ src/test/java/Java/InterviewQuestions/_017_SumItemsInArray.java
@@ -5,16 +5,30 @@ public class _017_SumItemsInArray {
 	public static void main(String[] args) {
 		int[] arr = { 15, 12, 13, 10 }; // 50
 		
-		System.out.println(_01_Using_Linear_Search(arr));
+		System.out.println(_01_Using_For_loop(arr));
+		
+		System.out.println(_02_Using_While_loop(arr));
 
 	}
 
-	private static int _01_Using_Linear_Search(int[] arr) {
+	private static int _01_Using_For_loop(int[] arr) {
 		// Time complexity: O(n)
 		// Space complexity: O(1)
 		int sum = 0;
 		for (int num : arr) {
 			sum+=num;
+		}
+		return sum;
+	}
+	
+	private static int _02_Using_While_loop(int[] arr) {
+		// Time complexity: O(n)
+		// Space complexity: O(1)
+		int sum = 0;
+		int i =0;
+		while(i<arr.length) {
+			sum+=arr[i];
+			i++;
 		}
 		return sum;
 	}
