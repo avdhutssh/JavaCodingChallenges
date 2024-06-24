b476754b973078c6e306f64eab93579ab076a630
avdhutssh <avdhut.ssh@gmail.com>
2024-06-24 19:10:59 +0530

    _016_smallest_LargestElementArray using linear search optimize

--- src/test/java/Java/InterviewQuestions/_016_smallest_LargestElementArray.java
+++ src/test/java/Java/InterviewQuestions/_016_smallest_LargestElementArray.java
@@ -1,11 +1,14 @@
 package Java.InterviewQuestions;
 
+import java.util.Arrays;
+
 public class _016_smallest_LargestElementArray {
 
 	public static void main(String[] args) {
 		int[] arr = { 2, 10, 10, 100, 2, 10, 11, 2, 1, 2 }; // smallest : 1, Largest : 100
 
 		_01_Using_Linear_Search(arr);
+		Arrays.stream(_02_Using_Linear_Search_Optimize(arr)).forEach(value -> System.out.print(value +" "));
 
 	}
 
@@ -23,4 +26,18 @@ public class _016_smallest_LargestElementArray {
 		System.out.println("Smallest number: " + smallest + " , Largest number: " + largest);
 	}
 
+	private static int[] _02_Using_Linear_Search_Optimize(int[] arr) {
+		if (arr == null || arr.length == 0) {
+			throw new IllegalArgumentException("Array is empty or null");
+		}
+
+		int smallest = arr[0];
+		int largest = arr[0];
+
+		for (int i = 1; i < arr.length; i++) {
+			largest = arr[i] >= largest ? arr[i] : largest;
+			smallest = arr[i] <= smallest ? arr[i] : smallest;
+		}
+		return new int[] { smallest, largest };
+	}
 }
