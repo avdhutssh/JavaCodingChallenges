7341454e8737e1629f009671d47fe92819280bcc
avdhutssh <avdhut.ssh@gmail.com>
2024-06-24 20:44:17 +0530

    sum of all elements in java using stream

--- src/test/java/Java/InterviewQuestions/_017_SumItemsInArray.java
+++ src/test/java/Java/InterviewQuestions/_017_SumItemsInArray.java
@@ -1,13 +1,15 @@
 package Java.InterviewQuestions;
 
+import java.util.Arrays;
+
 public class _017_SumItemsInArray {
 
 	public static void main(String[] args) {
 		int[] arr = { 15, 12, 13, 10 }; // 50
 		
-		System.out.println(_01_Using_For_loop(arr));
-		
+		System.out.println(_01_Using_For_loop(arr));	
 		System.out.println(_02_Using_While_loop(arr));
+		System.out.println(_03_Using_Streams(arr));
 
 	}
 
@@ -32,4 +34,11 @@ public class _017_SumItemsInArray {
 		}
 		return sum;
 	}
+	
+	private static int _03_Using_Streams(int[] arr) {
+		// Time complexity: O(n)
+		// Space complexity: O(1)
+	    return Arrays.stream(arr).sum();
+	}
+
 }
