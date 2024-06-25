5deb00240d6e598ecc841b868bccd961df0c06f5
avdhutssh <avdhut.ssh@gmail.com>
2024-06-25 12:02:36 +0530

    sort the elements in array using selection sort

--- src/test/java/Java/InterviewQuestions/_018_AscedingOrderArray.java
+++ src/test/java/Java/InterviewQuestions/_018_AscedingOrderArray.java
@@ -5,33 +5,54 @@ import java.util.Arrays;
 public class _018_AscedingOrderArray {
 
 	public static void main(String[] args) {
-		int[] arr = new int[] { 5, 2, 8, 7, 1 }; // 1 2 5 7 8
 		int[] arr1 = new int[] { 5, 2, 8, 7, 1 }; // 1 2 5 7 8
+		int[] arr2 = new int[] { 5, 2, 8, 7, 1 }; // 1 2 5 7 8
+		int[] arr3 = new int[] { 5, 2, 8, 7, 1 }; // 1 2 5 7 8
+		int[] arr4 = new int[] { 5, 2, 8, 7, 1 }; // 1 2 5 7 8
 
-		System.out.println(Arrays.toString(_01_Using_Arrays_Sort(arr)));
-		System.out.println("Bubble Sort: " + Arrays.toString(_02_Using_Bubble_Sort(arr1)));
+		System.out.println("Arrays.sort: " + Arrays.toString(_01_Using_Arrays_Sort(arr1)));
+		System.out.println("Bubble Sort: " + Arrays.toString(_02_Using_Bubble_Sort(arr2)));
+		System.out.println("Selection Sort: " + Arrays.toString(_02_Using_Bubble_Sort(arr3)));
 
 	}
 
 	static int[] _01_Using_Arrays_Sort(int[] arr) {
 		// Time complexity: O(nlog(n))
-		// Space complexity: O(n)
-		int[] sortedArr = arr.clone();
-		Arrays.sort(sortedArr);
-		return sortedArr;
+		// Space complexity: O(nlogn)
+		Arrays.sort(arr);
+		return arr;
 	}
 
 	static int[] _02_Using_Bubble_Sort(int[] arr) {
 		// Time complexity: O(n^2)
 		// Space complexity: O(1)
-		for (int i = 0; i < arr.length-1; i++) {
-			for (int j = 0; j < arr.length-i-1; j++) {
+		for (int i = 0; i < arr.length - 1; i++) {
+			for (int j = 0; j < arr.length - i - 1; j++) {
 				if (arr[j] > arr[j + 1]) {
 					int temp = arr[j];
 					arr[j] = arr[j + 1];
 					arr[j + 1] = temp;
 				}
 			}
+		}
+		return arr;
+	}
+
+	static int[] _03_Using_Selection_Sort(int[] arr) {
+		// Time complexity: O(n^2)
+		// Space complexity: O(1)
+		for (int i = 0; i < arr.length - 1; i++) {
+			int minIndex = 0;
+			for (int j = i + 1; j < arr.length - 1; j++) {
+				if (arr[j] < arr[minIndex]) {
+					minIndex = j;
+				}
+
+			}
+			int temp = arr[minIndex];
+			arr[minIndex] = arr[i];
+			arr[i] = temp;
+
 		}
 		return arr;
 	}
