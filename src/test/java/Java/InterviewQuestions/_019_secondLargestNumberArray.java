31c3dcaab7cf227069e8e5cca440456cdf088c06
avdhutssh <avdhut.ssh@gmail.com>
2024-06-25 16:47:48 +0530

    Second Largest Number in an Array using linear scan

---
+++ src/test/java/Java/InterviewQuestions/_019_secondLargestNumberArray.java
@@ -0,0 +1,27 @@
+package Java.InterviewQuestions;
+
+public class _019_secondLargestNumberArray {
+
+	public static void main(String[] args) {
+		int[] arr1 = { 2, 100, 10, 100, 2, 10, 11, 2, 11, 2 }; // 11
+		int[] arr2 = { 2, 100, 10, 50, 300 }; // 100
+
+		System.out.println(_01_Using_Linear_Scan(arr1));
+		System.out.println(_01_Using_Linear_Scan(arr2));
+
+	}
+
+	private static int _01_Using_Linear_Scan(int[] arr) {
+		int largest = 0, secondLargest = 0;
+		for (int num : arr) {
+			if (num > largest) {
+				secondLargest = largest;
+				largest = num;
+			} else if (num > secondLargest && num != largest) {
+				secondLargest = num;
+			}
+		}
+		return secondLargest;
+	}
+
+}
