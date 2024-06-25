0f97bf1b86fc2f3fa7b9d4dc92082b16038fb38f
avdhutssh <avdhut.ssh@gmail.com>
2024-06-25 11:12:20 +0530

    sort the elements in array using bubble sort

--- src/test/java/Java/InterviewQuestions/_018_AscedingOrderArray.java
+++ src/test/java/Java/InterviewQuestions/_018_AscedingOrderArray.java
@@ -6,8 +6,10 @@ public class _018_AscedingOrderArray {
 
 	public static void main(String[] args) {
 		int[] arr = new int[] { 5, 2, 8, 7, 1 }; // 1 2 5 7 8
+		int[] arr1 = new int[] { 5, 2, 8, 7, 1 }; // 1 2 5 7 8
 
 		System.out.println(Arrays.toString(_01_Using_Arrays_Sort(arr)));
+		System.out.println("Bubble Sort: " + Arrays.toString(_02_Using_Bubble_Sort(arr1)));
 
 	}
 
@@ -17,6 +19,20 @@ public class _018_AscedingOrderArray {
 		int[] sortedArr = arr.clone();
 		Arrays.sort(sortedArr);
 		return sortedArr;
+	}
 
+	static int[] _02_Using_Bubble_Sort(int[] arr) {
+		// Time complexity: O(n^2)
+		// Space complexity: O(1)
+		for (int i = 0; i < arr.length-1; i++) {
+			for (int j = 0; j < arr.length-i-1; j++) {
+				if (arr[j] > arr[j + 1]) {
+					int temp = arr[j];
+					arr[j] = arr[j + 1];
+					arr[j + 1] = temp;
+				}
+			}
+		}
+		return arr;
 	}
 }
