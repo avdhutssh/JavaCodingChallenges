5b75e7ef7ee07ddc327ca94d98699f7e857684f6
avdhutssh <avdhut.ssh@gmail.com>
2024-06-26 15:27:30 +0530

    Odd and Even Numbers from an Array using two arrays

--- src/test/java/Java/InterviewQuestions/_022_oddEvenNumbersArray.java
+++ src/test/java/Java/InterviewQuestions/_022_oddEvenNumbersArray.java
@@ -1,6 +1,7 @@
 package Java.InterviewQuestions;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class _022_oddEvenNumbersArray {
@@ -8,6 +9,7 @@ public class _022_oddEvenNumbersArray {
 	public static void main(String[] args) {
 		int[] arr = { 23, 55, 54, 9, 76, 66, 2, 91 }; // Even: 54 76 66 2 | Odd: 23 55 9 91
 		_01_Using_Linear_Scan(arr);
+		_02_Using_Two_Arrays(arr);
 	}
 
 	private static void _01_Using_Linear_Scan(int[] arr) {
@@ -23,5 +25,22 @@ public class _022_oddEvenNumbersArray {
 			}
 		}
 		System.out.println("Even: " + even + " | Odd: " + odd);
+	}
+
+	private static void _02_Using_Two_Arrays(int[] arr) {
+		// Time complexity: O(n)
+		// Space complexity: O(n)
+		int[] even = new int[arr.length];
+		int[] odd = new int[arr.length];
+		int evenCount = 0, oddCount = 0;
+		for (int num : arr) {
+			if (num % 2 == 0) {
+				even[evenCount++] = num;
+			} else {
+				odd[oddCount++] = num;
+			}
+		}
+		System.out.println("Even: " + Arrays.toString(Arrays.copyOf(even, evenCount)) + " | Odd: "
+				+ Arrays.toString(Arrays.copyOf(odd, oddCount)));
 	}
 }
