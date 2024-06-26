22ddfd0d290859081567604906c91430724ea03b
avdhutssh <avdhut.ssh@gmail.com>
2024-06-26 16:19:50 +0530

    remove all the white spaces in a string using stream

--- src/test/java/Java/InterviewQuestions/_023_removeAllWhiteSpacesFromString.java
+++ src/test/java/Java/InterviewQuestions/_023_removeAllWhiteSpacesFromString.java
@@ -1,13 +1,17 @@
 package Java.InterviewQuestions;
 
+import java.util.stream.Collectors;
+
 public class _023_removeAllWhiteSpacesFromString {
 
 	public static void main(String[] args) {
 		String str = "Hello World 123";
-		
+
 		System.out.println(_01_UsingReplaceAll(str));
-		
+
 		System.out.println(_02_Using_CharArray(str));
+		
+		System.out.println(_03_Using_Stream(str));
 
 	}
 
@@ -23,11 +27,15 @@ public class _023_removeAllWhiteSpacesFromString {
 		char[] arr = str.toCharArray();
 		StringBuffer sb = new StringBuffer();
 		for (char c : arr) {
-			if(c!=' ') {
+			if (c != ' ') {
 				sb.append(c);
 			}
 		}
 		return sb.toString();
 	}
-	
+
+	private static String _03_Using_Stream(String str) {
+		return str.chars().filter(c -> !Character.isWhitespace(c)).mapToObj(c -> String.valueOf((char) c))
+				.collect(Collectors.joining());
+	}
 }
