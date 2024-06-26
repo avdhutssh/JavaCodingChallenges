bf640c85beb5723decf773430c5ea989baab42d7
avdhutssh <avdhut.ssh@gmail.com>
2024-06-26 16:13:36 +0530

    remove all the white spaces in a string using char array and string buffer

--- src/test/java/Java/InterviewQuestions/_023_removeAllWhiteSpacesFromString.java
+++ src/test/java/Java/InterviewQuestions/_023_removeAllWhiteSpacesFromString.java
@@ -4,7 +4,10 @@ public class _023_removeAllWhiteSpacesFromString {
 
 	public static void main(String[] args) {
 		String str = "Hello World 123";
+		
 		System.out.println(_01_UsingReplaceAll(str));
+		
+		System.out.println(_02_Using_CharArray(str));
 
 	}
 
@@ -14,4 +17,17 @@ public class _023_removeAllWhiteSpacesFromString {
 		return str.replaceAll("\\s", "");
 	}
 
+	private static String _02_Using_CharArray(String str) {
+		// Time Complexity: O(n)
+		// Space Complexity: O(n)
+		char[] arr = str.toCharArray();
+		StringBuffer sb = new StringBuffer();
+		for (char c : arr) {
+			if(c!=' ') {
+				sb.append(c);
+			}
+		}
+		return sb.toString();
+	}
+	
 }
