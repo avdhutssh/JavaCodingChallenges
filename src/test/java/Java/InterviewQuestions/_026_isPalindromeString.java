2bcefb15f16bf94e389bbe4de347bfe01b0756ca
avdhutssh <avdhut.ssh@gmail.com>
2024-06-27 18:57:12 +0530

    Check whether string is palindrome by reversing string

--- src/test/java/Java/InterviewQuestions/_026_isPalindromeString.java
+++ src/test/java/Java/InterviewQuestions/_026_isPalindromeString.java
@@ -5,15 +5,22 @@ public class _026_isPalindromeString {
 	public static void main(String[] args) {
 		String str1 = "Madam"; // true
 		String str2 = "Avdhut"; // false
+		String str3 = "A man a plan a canal Panama";
 
 		System.out.println(_01_Using_Brute_force_Approach(str1));
 		System.out.println(_01_Using_Brute_force_Approach(str2));
+		System.out.println(_01_Using_Brute_force_Approach(str3));	
+		System.out.println("----------------------------------");
+		
+		System.out.println(_02_Using_String_Reverse(str1));
+		System.out.println(_02_Using_String_Reverse(str2));
+		System.out.println(_02_Using_String_Reverse(str3));
 	}
 
 	private static boolean _01_Using_Brute_force_Approach(String str) {
 		// Time Complexity: O(n)
 		// Space Complexity: O(1)
-		str = str.toLowerCase();
+		str = str.trim().replaceAll("\\s+","").toLowerCase();
 		for (int i = 0; i < str.length() / 2; i++) {
 			if (str.charAt(i) != str.charAt(str.length() - 1 - i)) {
 				return false;
@@ -22,4 +29,11 @@ public class _026_isPalindromeString {
 		return true;
 	}
 
+	private static boolean _02_Using_String_Reverse(String str) {
+		// Time Complexity: O(n)
+		// Space Complexity: O(n)
+		str = str.trim().replaceAll("\\s+","").toLowerCase();
+		String reverseStr= new StringBuilder(str).reverse().toString();
+		return str.equals(reverseStr);
+	}
 }
