e974c7dff123bacc873315dffdd447ff436c2594
avdhutssh <avdhut.ssh@gmail.com>
2024-06-28 11:16:08 +0530

    remove leading zeros from string using substring

--- src/test/java/Java/InterviewQuestions/_028_removeLeadingZeros.java
+++ src/test/java/Java/InterviewQuestions/_028_removeLeadingZeros.java
@@ -6,11 +6,18 @@ public class _028_removeLeadingZeros {
 		String str1 = "00000123569";// 123569
 		String str2 = "000012356090";// 12356090
 
-		System.out.println(_01_Using_Brute_Force_Approach(str1));
-		System.out.println(_01_Using_Brute_Force_Approach(str2));
+		System.out.println("Using Brute Force : " + _01_Using_Brute_Force_Approach(str1));
+		System.out.println("Using Brute Force : " + _01_Using_Brute_Force_Approach(str2));
+		System.out.println("-----------------------------------------------------------");
+
+		System.out.println("Using Substring : " + _02_Using_Substring(str1));
+		System.out.println("Using Substring : " + _02_Using_Substring(str2));
+		System.out.println("-----------------------------------------------------------");
 	}
 
 	private static String _01_Using_Brute_Force_Approach(String str) {
+		// Time Complexity: O(n)
+		// Space Complexity: O(n)
 		String result = "";
 		int start = 0;
 		for (int i = 0; i < str.length(); i++) {
@@ -26,4 +33,15 @@ public class _028_removeLeadingZeros {
 		return result;
 	}
 
+	private static String _02_Using_Substring(String str) {
+		// Time Complexity: O(n)
+		// Space Complexity: O(n)
+		for (int i = 0; i < str.length(); i++) {
+			if (str.charAt(i) != '0') {
+				return str.substring(i);
+			}
+		}
+		return str;
+
+	}
 }
