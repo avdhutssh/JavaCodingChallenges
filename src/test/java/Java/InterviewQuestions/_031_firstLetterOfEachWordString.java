e48e958d1c02b2ef4000a2060ceff36bff5c8f53
avdhutssh <avdhut.ssh@gmail.com>
2024-06-28 16:59:41 +0530

    first letter of each word in a string using split and loop

--- src/test/java/Java/InterviewQuestions/_031_firstLetterOfEachWordString.java
+++ src/test/java/Java/InterviewQuestions/_031_firstLetterOfEachWordString.java
@@ -3,9 +3,10 @@ package Java.InterviewQuestions;
 public class _031_firstLetterOfEachWordString {
 
 	public static void main(String[] args) {
-		String str = "geeks for geeks"; // gfg
+		String str = "  geeks for geeks   "; // gfg
 
-		System.out.println(_01_Using_BruteForceApproach(str));
+		System.out.println("Using BFA: " + _01_Using_BruteForceApproach(str));
+		System.out.println("Using spilt and loop: " + _02_Using_Split_Loop(str));
 
 	}
 
@@ -26,4 +27,17 @@ public class _031_firstLetterOfEachWordString {
 		return result;
 	}
 
+	private static String _02_Using_Split_Loop(String str) {
+		// Time Complexity: O(n)
+		// Space Complexity: O(n)
+		String result = "";
+		String[] words = str.split("\\s+");
+		for (String word : words) {
+			if (!word.isEmpty()) {
+				result += word.charAt(0);
+			}
+		}
+
+		return result;
+	}
 }
