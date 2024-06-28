f5babbc9457d081488162a242e91bc893b9c7439
avdhutssh <avdhut.ssh@gmail.com>
2024-06-28 15:43:15 +0530

    remove both leading and trailing zeros from string using brute force approach

---
+++ src/test/java/Java/InterviewQuestions/_030_removeLeading_And_Trailing_Zeros.java
@@ -0,0 +1,32 @@
+package Java.InterviewQuestions;
+
+import java.util.Iterator;
+
+public class _030_removeLeading_And_Trailing_Zeros {
+
+	public static void main(String[] args) {
+		String str1 = "1230456000000"; // 1230456
+		String str2 = "00001230456000"; // 1230456
+
+		System.out.println("Using BFA: " + _01_Using_Brute_Force_Approach(str1));
+		System.out.println("Using BFA: " + _01_Using_Brute_Force_Approach(str2));
+		System.out.println("----------------------------------------------------");
+
+	}
+
+	private static String _01_Using_Brute_Force_Approach(String str) {
+		// Time Complexity: O(n)
+		// Space Complexity: O(n)
+		int start =0;
+		int end = str.length()-1;
+		while(start<str.length()-1 && str.charAt(start)=='0') {
+			start++;
+		}
+		while(end>=0&& str.charAt(end)=='0') {
+			end--;
+		}
+
+		return str.substring(start,end+1);
+	}
+
+}
