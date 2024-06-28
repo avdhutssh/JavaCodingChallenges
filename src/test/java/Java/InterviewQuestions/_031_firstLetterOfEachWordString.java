479496ff694b8dce94cdf42777a53f48b0b3321a
avdhutssh <avdhut.ssh@gmail.com>
2024-06-28 17:04:55 +0530

    first letter of each word in a string using regular expression

--- src/test/java/Java/InterviewQuestions/_031_firstLetterOfEachWordString.java
+++ src/test/java/Java/InterviewQuestions/_031_firstLetterOfEachWordString.java
@@ -1,5 +1,8 @@
 package Java.InterviewQuestions;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class _031_firstLetterOfEachWordString {
 
 	public static void main(String[] args) {
@@ -7,6 +10,7 @@ public class _031_firstLetterOfEachWordString {
 
 		System.out.println("Using BFA: " + _01_Using_BruteForceApproach(str));
 		System.out.println("Using spilt and loop: " + _02_Using_Split_Loop(str));
+		System.out.println("Using RegX: " + _03_Using_RegX(str));
 
 	}
 
@@ -38,6 +42,17 @@ public class _031_firstLetterOfEachWordString {
 			}
 		}
 
+		return result;
+	}
+
+	private static String _03_Using_RegX(String str) {
+		// Time Complexity: O(n)
+		// Space Complexity: O(n)
+		String result = "";
+		Matcher matcher = Pattern.compile("\\b\\w").matcher(str);
+		while (matcher.find()) {
+			result += matcher.group();
+		}
 		return result;
 	}
 }
