2b03d71855d588cbf8e70b18b1f5f67cbd35c543
avdhutssh <avdhut.ssh@gmail.com>
2024-06-29 18:36:08 +0530

    fibonacci series using recursion

--- src/test/java/Java/InterviewQuestions/_34_fibonacci_Series.java
+++ src/test/java/Java/InterviewQuestions/_34_fibonacci_Series.java
@@ -10,11 +10,15 @@ public class _34_fibonacci_Series {
 		int num = 5; // 0,1,1,2,3
 
 		System.out.println(_01_Using_Brute_Force_Approach(num));
+		for (int i = 0; i < num; i++) {
+			System.out.print(_02_Using_Recursion(i) + " ");
+		}
 
 	}
 
 	private static List<Integer> _01_Using_Brute_Force_Approach(int num) {
-
+		// Time complexity: O(n)
+		// Space complexity: O(1)
 		int a = 1, b = 1, sum = 0;
 		List<Integer> fib = new ArrayList<Integer>();
 		for (int i = 1; i <= num; i++) {
@@ -28,6 +32,16 @@ public class _34_fibonacci_Series {
 			}
 		}
 		return fib;
+
+	}
+
+	private static int _02_Using_Recursion(int num) {
+		// Time complexity: O(2^n)
+		// Space complexity: O(1)
+		if (num <= 1) {
+			return num;
+		}
+		return _02_Using_Recursion(num - 1) + _02_Using_Recursion(num - 2);
 
 	}
 }
