f4d916217e99b76dfb2238bdf468fa63e6311e83
avdhutssh <avdhut.ssh@gmail.com>
2024-06-30 20:45:08 +0530

    reverse number using String Builder

--- src/test/java/Java/InterviewQuestions/_37_reverse_Number.java
+++ src/test/java/Java/InterviewQuestions/_37_reverse_Number.java
@@ -6,12 +6,16 @@ public class _37_reverse_Number {
 		int num1 = 456; // 654
 		int num2 = 899; // 998
 
-		System.out.println(_01_using_BruteForceApproach(num1));
-		System.out.println(_01_using_BruteForceApproach(num2));
+		System.out.println(_01_Using_BruteForceApproach(num1));
+		System.out.println(_01_Using_BruteForceApproach(num2));
+		System.out.println("--------------------------------");
 
+		System.out.println(_02_Using_String_Builder(num1));
+		System.out.println(_02_Using_String_Builder(num2));
+		System.out.println("--------------------------------");
 	}
 
-	private static int _01_using_BruteForceApproach(int num) {
+	private static int _01_Using_BruteForceApproach(int num) {
 		// Time complexity: O(n)
 		// Space complexity: O(1)
 		int reverseNum = 0;
@@ -21,5 +25,12 @@ public class _37_reverse_Number {
 			num /= 10;
 		}
 		return reverseNum;
+	}
+	
+	private static String _02_Using_String_Builder(int num) {
+		// Time complexity: O(n)
+		// Space complexity: O(1)
+		String numStr = Integer.toString(num);
+		return new StringBuilder(numStr).reverse().toString();
 	}
 }
