e6f800a8419fea3d9f2db95bec59c4824be3efe0
avdhutssh <avdhut.ssh@gmail.com>
2024-07-01 19:55:04 +0530

    swapping of 2 numbers without using temp variable

---
+++ src/test/java/Java/InterviewQuestions/_41_swapTwoNumbers.java
@@ -0,0 +1,23 @@
+package Java.InterviewQuestions;
+
+public class _41_swapTwoNumbers {
+
+	public static void main(String[] args) {
+		int num1 = 12;
+		int num2 = 24;
+		_01_Without_Using_Temp_Variable(num1, num2);
+	}
+
+	private static void _01_Without_Using_Temp_Variable(int num1, int num2) {
+		// Time complexity: O(n)
+		// Space complexity: O(n)
+		System.out.println("Before-> num1 : " + num1 + " num2 : " + num2);
+
+		num1 = num1 + num2;
+		num2 = num1 - num2;
+		num1 = num1 - num2;
+		System.out.println("After-> num1 : " + num1 + " num2 : " + num2);
+
+	}
+
+}
