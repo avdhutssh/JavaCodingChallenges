5c770d55db604eaa6c64022cfa8549218975963b
avdhutssh <avdhut.ssh@gmail.com>
2024-07-03 10:53:24 +0530

    count of Capitalized words in a String using regular expression

--- src/test/java/Java/InterviewQuestions/_43_countCapitalizedWordsInString.java
+++ src/test/java/Java/InterviewQuestions/_43_countCapitalizedWordsInString.java
@@ -1,5 +1,8 @@
 package Java.InterviewQuestions;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class _43_countCapitalizedWordsInString {
 
 	public static void main(String[] args) {
@@ -10,15 +13,32 @@ public class _43_countCapitalizedWordsInString {
 		System.out.println("Using BFA: " + _01_Using_BruteForceApproach(str2));
 		System.out.println("-------------------");
 
+		System.out.println("Using Regular Expresson: " + _02_Using_Regular_Expression(str1));
+		System.out.println("Using Regular Expresson: " + _02_Using_Regular_Expression(str2));
+		System.out.println("-------------------");
+
 	}
 
 	private static int _01_Using_BruteForceApproach(String str) {
-
+		// Time complexity: O(n)
+		// Space complexity: O(n)
 		int count = 0;
 		for (char ch : str.toCharArray()) {
 			if (Character.isUpperCase(ch)) {
 				count++;
 			}
+		}
+		return count;
+	}
+
+	private static int _02_Using_Regular_Expression(String str) {
+		// Time complexity: O(n)
+		// Space complexity: O(1)
+		int count = 0;
+		Pattern pattern = Pattern.compile("[A-Z]");
+		Matcher matcher = pattern.matcher(str);
+		while (matcher.find()) {
+			count++;
 		}
 		return count;
 	}
