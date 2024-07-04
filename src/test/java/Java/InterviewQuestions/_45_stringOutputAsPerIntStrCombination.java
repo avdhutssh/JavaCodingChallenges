8b3e3395efc24f065b6eb56d253226319d0a6ef4
avdhut-satish.shirgaonkar.ext <avdhut-satish.shirgaonkar.ext@siemens.com>
2024-07-04 18:22:25 +0530

    to generate Output aabbbcccc with the input a2b3c4 using BFA

---
+++ src/test/java/Java/InterviewQuestions/_45_stringOutputAsPerIntStrCombination.java
@@ -0,0 +1,28 @@
+package Java.InterviewQuestions;
+
+public class _45_stringOutputAsPerIntStrCombination {
+
+	public static void main(String[] args) {
+		String str = "a2b3c4"; // aabbbcccc
+
+		System.out.println("Using BFA: " + _01_Using_Brute_Force_Approach(str));
+
+	}
+
+	private static String _01_Using_Brute_Force_Approach(String str) {
+		// Time Complexity: O(n^2)
+		// Space Complexity: O(n)
+		String result = "";
+		for (int i = 0; i < str.length(); i++) {
+			if (Character.isAlphabetic(str.charAt(i))) {
+				result += str.charAt(i);
+			} else {
+				int num = Character.getNumericValue(str.charAt(i));
+				for (int j = 1; j < num; j++) {
+					result += str.charAt(i - 1);
+				}
+			}
+		}
+		return result;
+	}
+}
