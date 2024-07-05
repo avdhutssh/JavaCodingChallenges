9e6494dc28cc49e6dae949a324b9d0895f26562a
avdhutssh <avdhut.ssh@gmail.com>
2024-07-05 15:50:35 +0530

    missing number in a given Array using HashSet

--- src/test/java/Java/InterviewQuestions/_47_missingNumberInGivenArray.java
+++ src/test/java/Java/InterviewQuestions/_47_missingNumberInGivenArray.java
@@ -1,11 +1,14 @@
 package Java.InterviewQuestions;
 
+import java.util.HashSet;
+
 public class _47_missingNumberInGivenArray {
 
 	public static void main(String[] args) {
 		int[] arr = { 1, 2, 3, 5, 6 }; // 4
 
 		System.out.println("Using BFA : " + _01_Using_Brute_Force_Approach(arr));
+		System.out.println("Using HashSet : " + _02_Using_HashSet(arr));
 
 	}
 
@@ -21,4 +24,18 @@ public class _47_missingNumberInGivenArray {
 		return expectedSum - actualSum;
 	}
 
+	private static int _02_Using_HashSet(int[] arr) {
+		// Time Complexity: O(n)
+		// Space Complexity: O(n)
+		HashSet<Integer> hs = new HashSet<>();
+		for (int num : arr) {
+			hs.add(num);
+		}
+		for (int i = 1; i <= arr.length; i++) {
+			if (!hs.contains(i)) {
+				return i;
+			}
+		}
+		return -1;
+	}
 }
