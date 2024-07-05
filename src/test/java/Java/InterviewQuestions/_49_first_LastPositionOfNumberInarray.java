97bcbbc16ac8a767720d6eb911d8850abe289f71
avdhutssh <avdhut.ssh@gmail.com>
2024-07-05 18:18:38 +0530

    first and last position of a number in a given array using linear iteration

--- src/test/java/Java/InterviewQuestions/_49_first_LastPositionOfNumberInarray.java
+++ src/test/java/Java/InterviewQuestions/_49_first_LastPositionOfNumberInarray.java
@@ -10,10 +10,12 @@ public class _49_first_LastPositionOfNumberInarray {
 		int target = 8; // Output: [3, 4]
 
 		System.out.println("Using BFA: " + Arrays.toString(_01_Using_BruteForceApproach(arr, target)));
+		System.out.println("Using Linear Iteration: " + Arrays.toString(_02_Using_LinearIteration(arr, target)));
 	}
 
 	private static int[] _01_Using_BruteForceApproach(int[] arr, int target) {
-
+		// Time Complexity: O(n^2)
+		// Space Complexity: O(1)
 		for (int i = 0; i < arr.length; i++) {
 			if (arr[i] == target) {
 				for (int j = i + 1; j < arr.length; j++) {
@@ -25,6 +27,24 @@ public class _49_first_LastPositionOfNumberInarray {
 
 		}
 		return null;
+	}
+
+	private static int[] _02_Using_LinearIteration(int[] arr, int target) {
+		// Time Complexity: O(n)
+		// Space Complexity: O(1)
+		int first = -1;
+		int last = -1;
+
+		for (int i = 0; i < arr.length; i++) {
+			if (arr[i] == target) {
+				if (first == -1) {
+					first = i;
+				}
+				last = i;
+			}
+
+		}
+		return new int[] { first, last };
 	}
 
 }
