e65c7b4cc4bccb0c7632bf59880720e1179a8aa5
avdhutssh <avdhut.ssh@gmail.com>
2024-07-06 19:28:22 +0530

    shift all zeros on the right using In Stream

--- src/test/java/Java/InterviewQuestions/_51_shiftAllZerosOnRight.java
+++ src/test/java/Java/InterviewQuestions/_51_shiftAllZerosOnRight.java
@@ -1,15 +1,17 @@
 package Java.InterviewQuestions;
 
 import java.util.Arrays;
+import java.util.stream.IntStream;
 
 public class _51_shiftAllZerosOnRight {
 
 	public static void main(String[] args) {
 		int[] arr1 = { 1, 0, 2, 0, 3, 0, 0, 0 }; // 1, 2, 3, 0, 0, 0, 0, 0
+		int[] arr2 = { 1, 0, 2, 0, 3, 0, 0, 0 }; // 1, 2, 3, 0, 0, 0, 0, 0
 
 		System.out.println("Using Brute Force Approach: " + Arrays.toString(_01_Using_BruteForceApproach(arr1)));
 		System.out.println("Using In Place Approach: " + Arrays.toString(_02_Using_InPlaceApproach(arr1)));
-
+		System.out.println("Using In Stream: " + Arrays.toString(_03_Using_Stream(arr2)));
 	}
 
 	private static int[] _01_Using_BruteForceApproach(int[] arr) {
@@ -40,5 +42,13 @@ public class _51_shiftAllZerosOnRight {
 			arr[index++] = 0;
 		}
 		return arr;
+	}
+
+	private static int[] _03_Using_Stream(int[] arr) {
+		// Time Complexity: O(n)
+		// Space Complexity: O(n)
+		int[] nonZeros = Arrays.stream(arr).filter(num -> num != 0).toArray();
+		int[] zeros = new int[arr.length - nonZeros.length];
+		return IntStream.concat(Arrays.stream(nonZeros), Arrays.stream(zeros)).toArray();
 	}
 }
