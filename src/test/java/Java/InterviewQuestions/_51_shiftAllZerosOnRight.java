68f3693012d3754e51fbfe919c3951beb832c0eb
avdhutssh <avdhut.ssh@gmail.com>
2024-07-06 19:06:17 +0530

    shift all zeros on the right using In Place Approach

--- src/test/java/Java/InterviewQuestions/_51_shiftAllZerosOnRight.java
+++ src/test/java/Java/InterviewQuestions/_51_shiftAllZerosOnRight.java
@@ -8,6 +8,7 @@ public class _51_shiftAllZerosOnRight {
 		int[] arr1 = { 1, 0, 2, 0, 3, 0, 0, 0 }; // 1, 2, 3, 0, 0, 0, 0, 0
 
 		System.out.println("Using Brute Force Approach: " + Arrays.toString(_01_Using_BruteForceApproach(arr1)));
+		System.out.println("Using In Place Approach: " + Arrays.toString(_02_Using_InPlaceApproach(arr1)));
 
 	}
 
@@ -25,4 +26,19 @@ public class _51_shiftAllZerosOnRight {
 		return result;
 	}
 
+	private static int[] _02_Using_InPlaceApproach(int[] arr) {
+		// Time Complexity: O(n)
+		// Space Complexity: O(1)
+		int index = 0;
+		for (int i = 0; i < arr.length; i++) {
+			if (arr[i] != 0) {
+				arr[index++] = arr[i];
+			}
+
+		}
+		while (index < arr.length) {
+			arr[index++] = 0;
+		}
+		return arr;
+	}
 }
