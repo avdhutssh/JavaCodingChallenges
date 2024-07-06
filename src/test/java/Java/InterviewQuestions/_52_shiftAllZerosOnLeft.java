5187b216b67e139b10b5302d35c52135ce4bc8d6
avdhutssh <avdhut.ssh@gmail.com>
2024-07-06 20:01:05 +0530

    shift all zeros on the left using In Place Approach

--- src/test/java/Java/InterviewQuestions/_52_shiftAllZerosOnLeft.java
+++ src/test/java/Java/InterviewQuestions/_52_shiftAllZerosOnLeft.java
@@ -1,22 +1,21 @@
 package Java.InterviewQuestions;
 
 import java.util.Arrays;
-import java.util.Iterator;
 
 public class _52_shiftAllZerosOnLeft {
 
 	public static void main(String[] args) {
 		int[] arr1 = { 1, 0, 2, 0, 3, 0, 0, 0 }; // 0, 0, 0, 0, 0, 1, 2, 3
-		
 
 		System.out.println("Using Brute Force Approach: " + Arrays.toString(_01_Using_BruteForceApproach(arr1)));
+		System.out.println("Using In Place Approach: " + Arrays.toString(_02_Using_InPlaceApproach(arr1)));
 
 	}
 
 	private static int[] _01_Using_BruteForceApproach(int[] arr) {
 		// Time Complexity: O(n)
 		// Space Complexity: O(n)
-		int index = arr.length;
+		int index = arr.length-1;
 		int[] result = new int[arr.length];
 		for (int i = arr.length - 1; i >= 0; i--) {
 			if (arr[i] != 0) {
@@ -26,4 +25,19 @@ public class _52_shiftAllZerosOnLeft {
 		return result;
 	}
 
+	private static int[] _02_Using_InPlaceApproach(int[] arr) {
+		// Time Complexity: O(n)
+		// Space Complexity: O(1)
+		int index = arr.length - 1;
+		for (int i = arr.length - 1; i >= 0; i--) {
+			if (arr[i] != 0) {
+				arr[index--] = arr[i];
+			}
+
+		}
+		while (index >= 0) {
+			arr[index--] = 0;
+		}
+		return arr;
+	}
 }
