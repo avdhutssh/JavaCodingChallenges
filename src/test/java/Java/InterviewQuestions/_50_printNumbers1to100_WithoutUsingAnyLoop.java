8a32112a7e1f07fe74ff6fccdd5f78fcf6c3626d
avdhutssh <avdhut.ssh@gmail.com>
2024-07-06 18:38:55 +0530

    print numbers 1 to 100 without using any loop using Stream

--- src/test/java/Java/InterviewQuestions/_50_printNumbers1to100_WithoutUsingAnyLoop.java
+++ src/test/java/Java/InterviewQuestions/_50_printNumbers1to100_WithoutUsingAnyLoop.java
@@ -1,10 +1,15 @@
 package Java.InterviewQuestions;
 
+import java.util.stream.IntStream;
+
 public class _50_printNumbers1to100_WithoutUsingAnyLoop {
 
 	public static void main(String[] args) {
 
 		_01_Using_Recursion(1);
+		System.out.println("\n"
+				+ "---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------");
+		_02_Using_Stream();
 		System.out.println("\n"
 				+ "---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------");
 
@@ -17,5 +22,11 @@ public class _50_printNumbers1to100_WithoutUsingAnyLoop {
 			System.out.print(n + " ");
 			_01_Using_Recursion(n + 1);
 		}
+	}
+
+	private static void _02_Using_Stream() {
+		// Time Complexity: O(n)
+		// Space Complexity: O(n)
+		IntStream.rangeClosed(1, 100).forEach(n -> System.out.print(n + " "));
 	}
 }
