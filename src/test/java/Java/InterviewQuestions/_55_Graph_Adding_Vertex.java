eca8020590a39c44d328053193dbe7280a235190
avdhutssh <avdhut.ssh@gmail.com>
2024-07-07 13:42:21 +0530

    graph - add vertex

---
+++ src/test/java/Java/InterviewQuestions/_55_Graph_Adding_Vertex.java
@@ -0,0 +1,33 @@
+package Java.InterviewQuestions;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class _55_Graph_Adding_Vertex {
+
+	private HashMap<String, ArrayList<String>> adjList = new HashMap<>();
+
+	public void printGraph() {
+		System.out.println(adjList);
+	}
+
+	public boolean addVertex(String vertex) {
+		if (adjList.get(vertex) == null) {
+			adjList.put(vertex, new ArrayList<String>());
+			return true;
+		}
+		return false;
+
+	}
+
+	public static void main(String[] args) {
+
+		_55_Graph_Adding_Vertex graph = new _55_Graph_Adding_Vertex();
+		graph.printGraph();
+
+		graph.addVertex("A");
+
+		graph.printGraph();
+	}
+
+}
