582777a8a4109aa72c22e7c6dc83eb94bdf57bd3
avdhutssh <avdhut.ssh@gmail.com>
2024-07-11 16:24:45 +0530

    reverse the second half of Array using BFA

---
+++ src/test/java/Java/InterviewQuestions/_67_Reverse_Second_Half_Array.java
@@ -0,0 +1,31 @@
+package Java.InterviewQuestions;
+
+import java.util.Arrays;
+
+public class _67_Reverse_Second_Half_Array {
+
+	public static void main(String[] args) {
+		int[] arr1 = new int[] { 1, 2, 3, 4, 5, 6, 7 }; // {1, 2, 3, 4, 7, 6, 5 }
+		int[] arr2 = new int[] { 1, 2, 3, 4, 5, 6 }; // {1, 2, 3, 6, 5, 4 }
+
+		System.out.println("Using BFA : " + Arrays.toString(_01_Using_BruteForceApproach(arr1)));
+		System.out.println("Using BFA : " + Arrays.toString(_01_Using_BruteForceApproach(arr2)));
+	}
+
+	private static int[] _01_Using_BruteForceApproach(int[] arr) {
+
+		int[] result = new int[arr.length];
+		int mid = arr.length / 2;
+		int j = mid;
+		for (int i = arr.length - 1; i >= mid; i--) {
+			result[j++] = arr[i];
+
+		}
+		for (int i = 0; i < mid; i++) {
+			result[i] = arr[i];
+
+		}
+		return result;
+
+	}
+}
