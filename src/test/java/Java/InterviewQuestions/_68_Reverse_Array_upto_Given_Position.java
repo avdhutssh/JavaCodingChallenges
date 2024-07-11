d9d92250cb1ecba6f7bf43556a28017d18266ce2
avdhutssh <avdhut.ssh@gmail.com>
2024-07-11 20:19:57 +0530

    reverse the Array upto given position using Two Pointer Approach

--- src/test/java/Java/InterviewQuestions/_68_Reverse_Array_upto_Given_Position.java
+++ src/test/java/Java/InterviewQuestions/_68_Reverse_Array_upto_Given_Position.java
@@ -8,14 +8,17 @@ public class _68_Reverse_Array_upto_Given_Position {
 	public static void main(String[] args) {
 		int[] arr1 = new int[] { 1, 2, 3, 4, 5, 6, 7 }; // {5, 4, 3, 2, 1, 6, 7 }
 		int[] arr2 = new int[] { 1, 2, 3, 4, 5, 6 }; // {5, 4, 3, 2, 1, 6 }
+		int[] arr3 = new int[] { 1, 2, 3, 4, 5, 6 }; // {5, 4, 3, 2, 1, 6 }
 
 		System.out.println("Using BFA : " + Arrays.toString(_01_Using_BruteForceApproach(arr1, 5)));
 		System.out.println("Using BFA : " + Arrays.toString(_01_Using_BruteForceApproach(arr2, 5)));
 
 		System.out
 				.println("Using Simple loop with temp : " + Arrays.toString(_02_Using_Simple_Iteration_Temp(arr1, 5)));
-		
+
 		System.out.println("Using Stack : " + Arrays.toString(_03_Using_Stack(arr2, 5)));
+		
+		System.out.println("Using Two Pointer Approach : " + Arrays.toString(_04_Using_Two_Pointer_Approach(arr3, 5)));
 	}
 
 	private static int[] _01_Using_BruteForceApproach(int[] arr, int position) {
@@ -53,6 +56,21 @@ public class _68_Reverse_Array_upto_Given_Position {
 		}
 		for (int i = 0; i < position; i++) {
 			arr[i] = stack.pop();
+		}
+		return arr;
+	}
+
+	private static int[] _04_Using_Two_Pointer_Approach(int[] arr, int position) {
+		// Time Complexity: O(n)
+		// Space Complexity: O(1)
+		int start = 0;
+		int end = position - 1;
+		while (start < end) {
+			int temp = arr[start];
+			arr[start] = arr[end];
+			arr[end] = temp;
+			end--;
+			start++;
 		}
 		return arr;
 	}
