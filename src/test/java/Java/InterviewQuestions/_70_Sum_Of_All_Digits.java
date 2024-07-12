02513ebeaef65cb236b3bab3f419b86b695c16b6
avdhutssh <avdhut.ssh@gmail.com>
2024-07-12 11:05:35 +0530

    Sum of Digits of a Number Using String Conversion

--- src/test/java/Java/InterviewQuestions/_70_Sum_Of_All_Digits.java
+++ src/test/java/Java/InterviewQuestions/_70_Sum_Of_All_Digits.java
@@ -6,7 +6,7 @@ public class _70_Sum_Of_All_Digits {
 		int num = 876; // 21
 		System.out.println("Using Iterative Method: " + _01_Using_Iterative_Method(num));
 		System.out.println("Using Recursion Method: " + _02_Using_Recursion(num));
-
+		System.out.println("Using String Conversion: " + _03_Using_String_Conversion(num));
 	}
 
 	private static int _01_Using_Iterative_Method(int num) {
@@ -28,5 +28,17 @@ public class _70_Sum_Of_All_Digits {
 			return 0;
 		}
 		return num % 10 + _02_Using_Recursion(num / 10);
+	}
+
+	private static int _03_Using_String_Conversion(int num) {
+		// Time Complexity: O(n)
+		// Space Complexity: O(n)
+		String numStr = Integer.toString(num);
+		int sum = 0;
+		for (Character ch : numStr.toCharArray()) {
+			sum += ch - '0';
+//			sum += Character.getNumericValue(ch);
+		}
+		return sum;
 	}
 }
