b7babc4558bf646ad5908c218553b2a7f21e9533
avdhutssh <avdhut.ssh@gmail.com>
2024-07-13 17:34:18 +0530

    Median of array using Heap

--- src/test/java/Java/InterviewQuestions/_71_Median_Of_Array.java
+++ src/test/java/Java/InterviewQuestions/_71_Median_Of_Array.java
@@ -1,6 +1,8 @@
 package Java.InterviewQuestions;
 
 import java.util.Arrays;
+import java.util.Comparator;
+import java.util.PriorityQueue;
 
 public class _71_Median_Of_Array {
 
@@ -12,9 +14,14 @@ public class _71_Median_Of_Array {
 
 		int[] nums1 = { 1, 3, 4, 2, 7, 5 }; // 3.5
 		int[] nums2 = { 3, 5, 1, 4, 2 }; // 3
+		int[] nums3 = { 1, 3, 4, 2, 7, 5 }; // 3.5
+		int[] nums4 = { 3, 5, 1, 4, 2 }; // 3
 
-		System.out.println(_01_By_Sorting_Array(nums1));
-		System.out.println(_01_By_Sorting_Array(nums2));
+		System.out.println("Using Sorting: "+_01_By_Sorting_Array(nums1));
+		System.out.println("Using Sorting: "+_01_By_Sorting_Array(nums2));
+
+		System.out.println("Using Heap: "+_02_Using_Heap(nums3));
+		System.out.println("Using Heap: "+_02_Using_Heap(nums4));
 	}
 
 	private static double _01_By_Sorting_Array(int[] nums) {
@@ -29,4 +36,24 @@ public class _71_Median_Of_Array {
 		}
 	}
 
+	private static double _02_Using_Heap(int[] nums) {
+		// Time Complexity: O(logn)
+		// Space Complexity: O(nlogn)
+//		PriorityQueue<Integer> maxHeap = new PriorityQueue<Integer>((a, b) -> b - a);
+		PriorityQueue<Integer> maxHeap = new PriorityQueue<Integer>(Comparator.reverseOrder());
+		PriorityQueue<Integer> minHeap = new PriorityQueue<Integer>();
+
+		for (int num : nums) {
+			maxHeap.offer(num);
+			minHeap.offer(maxHeap.poll());
+			if (maxHeap.size() < minHeap.size()) {
+				maxHeap.offer(minHeap.poll());
+			}
+		}
+		if (maxHeap.size() > minHeap.size()) {
+			return maxHeap.peek();
+		} else {
+			return (maxHeap.peek() + minHeap.peek()) / 2.0;
+		}
+	}
 }
