ad529482247dc2fc4c37581dc584f641ee64da2d
avdhutssh <avdhut.ssh@gmail.com>
2024-07-16 12:05:20 +0530

    Remove a SubList from a List using remove range

--- src/test/java/Java/InterviewQuestions/_75_Remove_Sublist_From_List.java
+++ src/test/java/Java/InterviewQuestions/_75_Remove_Sublist_From_List.java
@@ -6,42 +6,45 @@ import java.util.List;
 
 public class _75_Remove_Sublist_From_List {
 
-	private static final int CustomArrayList = 0;
+    public static void main(String[] args) {
+        // Remove all the elements in the sublist whose index is between fromIndex,
+        // inclusive, and toIndex, exclusive
 
-	public static void main(String[] args) {
-		// Remove all the elements in the sublist whose index is between fromIndex,
-		// inclusive, and toIndex, exclusive
+        List<Integer> list1 = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8)); // fromIndex = 2, endIndex = 4
+        List<Integer> list2 = new CustomArrayList<>(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8)); // fromIndex = 2, endIndex = 4
+        // Output [1, 2, 5, 6, 7, 8]
 
-		List<Integer> list1 = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8)); // fromIndex = 2, endIndex = 4
-		List<Integer> list2 = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8)); // fromIndex = 2, endIndex = 4
-		List<Integer> list3 = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8)); // fromIndex = 2, endIndex = 4
-		// Output [1, 2, 5, 6, 7, 8]
+        System.out.println("Using Sublist and Clear Method: " + _01_Using_Sublist_And_Clear_Method(list1, 2, 4));
+        System.out.println("Using Remove Range Method: " + _02_Using_RemoveRange_Method(list2, 2, 4));
+    }
 
-		System.out.println("Using Sublist and Clear Method: " + _01_Using_Sublist_And_Clear_Method(list1, 2, 4));
-		System.out.println("Using Remove Range Method: " + _02_Using_RemoveRange_Method(list2, 2, 4));
+    private static List<Integer> _01_Using_Sublist_And_Clear_Method(List<Integer> list, int start, int end) {
+        // Time Complexity: O(n)
+        // Space Complexity: O(1)
+        list.subList(start, end).clear();
+        return list;
+    }
 
-	}
+    private static List<Integer> _02_Using_RemoveRange_Method(List<Integer> list, int start, int end) {
+        // Time Complexity: O(n)
+        // Space Complexity: O(1)
+        CustomArrayList<Integer> customList = (CustomArrayList<Integer>) list;
+        customList.removeRange(start, end);
+        return customList;
+    }
 
-	private static List<Integer> _01_Using_Sublist_And_Clear_Method(List<Integer> list, int start, int end) {
-		// Time Complexity: O(n)
-		// Space Complexity: O(1)
-		list.subList(start, end).clear();
-		return list;
-	}
+    static class CustomArrayList<E> extends ArrayList<E> {
+        public CustomArrayList() {
+            super();
+        }
 
-	private static List<Integer> _02_Using_RemoveRange_Method(List<Integer> list, int start, int end) {
-		// Time Complexity: O(n)
-		// Space Complexity: O(1)
-		CustomArrayList<Integer> customList = (CustomArrayList<Integer>) list;
-		customList.removeRange(start, end);
-		return customList;
+        public CustomArrayList(List<E> list) {
+            super(list);
+        }
 
-	}
-
-	static class CustomArrayList<E> extends ArrayList<E> {
-		public void removeRange(int fromIndex, int toIndex) {
-			super.removeRange(fromIndex, toIndex);
-		}
-
-	}
+        @Override
+        protected void removeRange(int fromIndex, int toIndex) {
+            super.removeRange(fromIndex, toIndex);
+        }
+    }
 }
