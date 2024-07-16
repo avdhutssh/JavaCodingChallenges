2187b3214a82882cd3cd74b1aeacc20dcaa652d7
avdhutssh <avdhut.ssh@gmail.com>
2024-07-16 12:18:08 +0530

    Remove a SubList from a List using Iteration

--- src/test/java/Java/InterviewQuestions/_75_Remove_Sublist_From_List.java
+++ src/test/java/Java/InterviewQuestions/_75_Remove_Sublist_From_List.java
@@ -14,13 +14,15 @@ public class _75_Remove_Sublist_From_List {
 		// inclusive, and toIndex, exclusive
 
 		List<Integer> list1 = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8));
-		List<Integer> list2 = new CustomArrayList<>(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8)); 
-		List<Integer> list3 = new CustomArrayList<>(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8)); 
-		 // fromIndex = 2, endIndex = 4 , Output [1, 2, 5, 6, 7, 8]
+		List<Integer> list2 = new CustomArrayList<>(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8));
+		List<Integer> list3 = new CustomArrayList<>(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8));
+		List<Integer> list4 = new CustomArrayList<>(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8));
+		// fromIndex = 2, endIndex = 4 , Output [1, 2, 5, 6, 7, 8]
 
 		System.out.println("Using Sublist and Clear Method: " + _01_Using_Sublist_And_Clear_Method(list1, 2, 4));
 		System.out.println("Using Remove Range Method: " + _02_Using_RemoveRange_Method(list2, 2, 4));
 		System.out.println("Using Linked List: " + _03_Using_LinkedList(list3, 2, 4));
+		System.out.println("Using Iteration: " + _04_Using_Iteration(list4, 2, 4));
 	}
 
 	private static List<Integer> _01_Using_Sublist_And_Clear_Method(List<Integer> list, int start, int end) {
@@ -62,5 +64,14 @@ public class _75_Remove_Sublist_From_List {
 			ll.remove(start);
 		}
 		return ll;
+	}
+
+	private static List<Integer> _04_Using_Iteration(List<Integer> list, int start, int end) {
+		// Time Complexity: O(n)
+		// Space Complexity: O(1)
+		for (int i = start; i < end; i++) {
+			list.remove(start);
+		}
+		return list;
 	}
 }
