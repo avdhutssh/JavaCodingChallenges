ed6a48406d7c5aabe466cddf378e10f72f80df7a
avdhutssh <avdhut.ssh@gmail.com>
2024-07-25 18:12:02 +0530

    second most frequent character in string using hashmap and sorting list

--- src/test/java/Java/InterviewQuestions/_081_Second_Most_frequent_Character_In_String.java
+++ src/test/java/Java/InterviewQuestions/_081_Second_Most_frequent_Character_In_String.java
@@ -1,6 +1,9 @@
 package Java.InterviewQuestions;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.Map.Entry;
 
 public class _081_Second_Most_frequent_Character_In_String {
@@ -13,6 +16,8 @@ public class _081_Second_Most_frequent_Character_In_String {
 		System.out.println(_01_Using_HashMap(str1));
 		System.out.println(_01_Using_HashMap(str2));
 
+		System.out.println(_02_Using_HashMap_And_Sorting(str1));
+		System.out.println(_02_Using_HashMap_And_Sorting(str2));
 	}
 
 	private static char _01_Using_HashMap(String str) {
@@ -33,11 +38,28 @@ public class _081_Second_Most_frequent_Character_In_String {
 			}
 		}
 		for (Entry<Character, Integer> entry : hm.entrySet()) {
-			if(entry.getValue()==secondMaxFreq) {
+			if (entry.getValue() == secondMaxFreq) {
 				return entry.getKey();
 			}
 		}
-			return 0;
+		return 0;
+
+	}
+
+	private static char _02_Using_HashMap_And_Sorting(String str) {
+		// Time Complexity: O(nlogn)
+		// Space Complexity: O(n)
+		HashMap<Character, Integer> hm = new HashMap<>();
+		for (char ch : str.toCharArray()) {
+			hm.put(ch, hm.getOrDefault(ch, 0) + 1);
+		}
+		List<Map.Entry<Character, Integer>> sortedEntries = new ArrayList<>(hm.entrySet());
+		sortedEntries.sort((e1, e2) -> e2.getValue().compareTo(e1.getValue()));
+		if(sortedEntries.size()>1) {
+			return sortedEntries.get(1).getKey();
+		}else {
+			throw new IllegalArgumentException("No second most frequent character found.");
+		}
 
 	}
 
