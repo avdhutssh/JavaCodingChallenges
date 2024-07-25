65d654b684c7e550fcb19ece8538e7cd137d08c2
avdhutssh <avdhut.ssh@gmail.com>
2024-07-25 20:02:19 +0530

    frequency of character in a string using array

--- src/test/java/Java/InterviewQuestions/_082_Frequency_Of_Character_In_String.java
+++ src/test/java/Java/InterviewQuestions/_082_Frequency_Of_Character_In_String.java
@@ -1,6 +1,8 @@
 package Java.InterviewQuestions;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
 public class _082_Frequency_Of_Character_In_String {
 
@@ -9,6 +11,7 @@ public class _082_Frequency_Of_Character_In_String {
 		String str = "DevLabsAlliance";
 
 		System.out.println("Using HashMap: " + _01_Using_HashMap(str));
+		_02_Using_Array(str);
 
 	}
 
@@ -20,6 +23,26 @@ public class _082_Frequency_Of_Character_In_String {
 			hm.put(ch, hm.getOrDefault(ch, 0) + 1);
 		}
 		return hm;
+	}
+
+	private static void _02_Using_Array(String str) {
+		// Time Complexity: O(n)
+		// Space Complexity: O(n)
+		str = str.toLowerCase();
+		int[] charCount = new int[256];
+		List<Character> unique = new ArrayList<>();
+		for (char ch : str.toCharArray()) {
+			charCount[ch]++;
+		}
+		System.out.println("Using Array: ");
+		for (char ch : str.toCharArray()) {
+			if (charCount[ch] > 0) {
+				if (!unique.contains(ch)) {
+					System.out.print(ch + "=" + charCount[ch] + " , ");
+					unique.add(ch);
+				}
+			}
+		}
 	}
 
 }
