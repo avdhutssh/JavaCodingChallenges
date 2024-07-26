6e8d347d6dacc1bd62c3da3698d552001d1acd00
avdhutssh <avdhut.ssh@gmail.com>
2024-07-26 18:08:45 +0530

    sort Strings in Lexicographical (Dictionary) order using CompareTo and array using spilt method

--- src/test/java/Java/InterviewQuestions/_083_Sort_Strings_In_Lexicographical_Order.java
+++ src/test/java/Java/InterviewQuestions/_083_Sort_Strings_In_Lexicographical_Order.java
@@ -7,10 +7,12 @@ public class _083_Sort_Strings_In_Lexicographical_Order {
 	public static void main(String[] args) {
 		String[] strArray1 = { "Harit", "girish", "Gritav", "Lovenish", "Nikhil", "Harman" };
 		String[] strArray2 = { "Harit", "girish", "Gritav", "Lovenish", "Nikhil", "Harman" };
+		String str3 = "Harit girish Gritav Lovenish Nikhil Harman";
 		// girish Gritav Harit Harman Lovenish Nikhil
 
 		System.out.println("Using CompareTo Method: " + Arrays.toString(_01_Using_Compare_Method(strArray1)));
 		System.out.println("Using Arrays.sort: " + Arrays.toString(_02_Using_Arrays_Sort(strArray2)));
+		System.out.println("Using Array and CompareTo method: " + _03_Using_Array_Compare_Method(str3));
 
 	}
 
@@ -29,12 +31,20 @@ public class _083_Sort_Strings_In_Lexicographical_Order {
 		}
 		return strArr;
 	}
-	
+
 	private static String[] _02_Using_Arrays_Sort(String[] strArr) {
-		// Time Complexity: O(n^2)
+		// Time Complexity: O(nlogn)
 		// Space Complexity: O(1)
 		System.out.println("Before Arrays.sort: " + Arrays.toString(strArr));
 		Arrays.sort(strArr, String.CASE_INSENSITIVE_ORDER);
 		return strArr;
+	}
+
+	private static String _03_Using_Array_Compare_Method(String str) {
+		// Time Complexity: O(n^2)
+		// Space Complexity: O(1)
+		String[] strArr = str.split("\\s+");
+		System.out.println("Before Array and Compare Method:" + Arrays.toString(strArr));
+		return Arrays.toString(_01_Using_Compare_Method(strArr));
 	}
 }
