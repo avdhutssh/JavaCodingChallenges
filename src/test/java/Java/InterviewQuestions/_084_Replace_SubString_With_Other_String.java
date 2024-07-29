450bc0ea6d20f24b42eff572f71ec592a1b8d347
avdhutssh <avdhut.ssh@gmail.com>
2024-07-29 17:47:39 +0530

    replace the Substring with another string using replaceAll

--- src/test/java/Java/InterviewQuestions/_084_Replace_SubString_With_Other_String.java
+++ src/test/java/Java/InterviewQuestions/_084_Replace_SubString_With_Other_String.java
@@ -9,6 +9,7 @@ public class _084_Replace_SubString_With_Other_String {
 
 		System.out.println(_01_Using_Array(str1, toBeReplaced, toReplacedWith));
 		System.out.println(_02_Using_Replace(str1, toBeReplaced, toReplacedWith));
+		System.out.println(_03_Using_ReplaceAll(str1, toBeReplaced, toReplacedWith));
 
 	}
 
@@ -33,5 +34,12 @@ public class _084_Replace_SubString_With_Other_String {
 		// Space Complexity: O(n)
 		System.out.println("Before Using Replace: " + str);
 		return str.replace(target, replacement);
+	}
+	
+	private static String _03_Using_ReplaceAll(String str, String target, String replacement) {
+		// Time Complexity: O(n)
+		// Space Complexity: O(n)
+		System.out.println("Before Using ReplaceAll: " + str);
+		return str.replaceAll(target, replacement);
 	}
 }
