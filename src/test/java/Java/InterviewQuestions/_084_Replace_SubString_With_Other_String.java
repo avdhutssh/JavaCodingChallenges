abcec56e4012af29c3a40fac4e154ab919e96142
avdhutssh <avdhut.ssh@gmail.com>
2024-07-29 18:27:01 +0530

    replace the Substring with another string using indexOf method

--- src/test/java/Java/InterviewQuestions/_084_Replace_SubString_With_Other_String.java
+++ src/test/java/Java/InterviewQuestions/_084_Replace_SubString_With_Other_String.java
@@ -7,16 +7,16 @@ public class _084_Replace_SubString_With_Other_String {
 		String toBeReplaced = "in";
 		String toReplacedWith = "with";
 
-		System.out.println(_01_Using_Array(str1, toBeReplaced, toReplacedWith));
-		System.out.println(_02_Using_Replace(str1, toBeReplaced, toReplacedWith));
-		System.out.println(_03_Using_ReplaceAll(str1, toBeReplaced, toReplacedWith));
+		System.out.println("Using Array and split: " + _01_Using_Array(str1, toBeReplaced, toReplacedWith));
+		System.out.println("Using Replace method: " + _02_Using_Replace(str1, toBeReplaced, toReplacedWith));
+		System.out.println("Using ReplaceAll method: " + _03_Using_ReplaceAll(str1, toBeReplaced, toReplacedWith));
+		System.out.println("Using indexOf method: " + _04_Using_IndexOf_Method(str1, toBeReplaced, toReplacedWith));
 
 	}
 
 	private static String _01_Using_Array(String str, String toBeReplaced, String toReplacedWith) {
 		// Time Complexity: O(n+k)
 		// Space Complexity: O(n)
-		System.out.println("Before Using Array: " + str);
 		String[] strArr = str.split(toBeReplaced);
 		String result = "";
 		for (int i = 0; i <= strArr.length - 1; i++) {
@@ -32,14 +32,28 @@ public class _084_Replace_SubString_With_Other_String {
 	private static String _02_Using_Replace(String str, String target, String replacement) {
 		// Time Complexity: O(n)
 		// Space Complexity: O(n)
-		System.out.println("Before Using Replace: " + str);
 		return str.replace(target, replacement);
 	}
-	
+
 	private static String _03_Using_ReplaceAll(String str, String target, String replacement) {
 		// Time Complexity: O(n)
-		// Space Complexity: O(n)
-		System.out.println("Before Using ReplaceAll: " + str);
+		// Space Complexity: O(n);
 		return str.replaceAll(target, replacement);
+	}
+
+	private static String _04_Using_IndexOf_Method(String str, String target, String replacement) {
+		// Time Complexity: O(n+m*k)
+		// Space Complexity: O(n)
+		StringBuilder result = new StringBuilder();
+		int targetLen = target.length();
+		int start = 0;
+		int index;
+
+		while ((index = str.indexOf(target, start)) != -1) {
+			result.append(str, start, index).append(replacement);
+			start = index + targetLen;
+		}
+		result.append(str.substring(start));
+		return result.toString();
 	}
 }
