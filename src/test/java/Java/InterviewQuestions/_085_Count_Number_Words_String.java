9d547c4bd571aff1366e3d7a6329f1baa4d1e56e
avdhutssh <avdhut.ssh@gmail.com>
2024-08-01 12:11:24 +0530

    count the number of words in a string using trim and split

--- src/test/java/Java/InterviewQuestions/_085_Count_Number_Words_String.java
+++ src/test/java/Java/InterviewQuestions/_085_Count_Number_Words_String.java
@@ -3,8 +3,15 @@ package Java.InterviewQuestions;
 public class _085_Count_Number_Words_String {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		String str = "    India Is My Country";
 
+		System.out.println(_01_Using_Split_Method(str));
+	}
+
+	private static int _01_Using_Split_Method(String str) {
+		// Time Complexity: O(n)
+		// Space Complexity: O(n)
+		return str.trim().split("\\s+").length;
 	}
 
 }
