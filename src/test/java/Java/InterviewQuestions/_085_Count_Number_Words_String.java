5a213ef55182eb3d00bece09803a1a84439dce17
avdhutssh <avdhut.ssh@gmail.com>
2024-08-01 16:53:13 +0530

    count the number of words in a string using iteration

--- src/test/java/Java/InterviewQuestions/_085_Count_Number_Words_String.java
+++ src/test/java/Java/InterviewQuestions/_085_Count_Number_Words_String.java
@@ -5,7 +5,8 @@ public class _085_Count_Number_Words_String {
 	public static void main(String[] args) {
 		String str = "    India Is My Country";
 
-		System.out.println(_01_Using_Split_Method(str));
+		System.out.println("Using Trim and Split method: " + _01_Using_Split_Method(str));
+		System.out.println("Using Iteration: " + _02_Using_Iteration(str));
 	}
 
 	private static int _01_Using_Split_Method(String str) {
@@ -14,4 +15,26 @@ public class _085_Count_Number_Words_String {
 		return str.trim().split("\\s+").length;
 	}
 
+	private static int _02_Using_Iteration(String str) {
+		// Time Complexity: O(n)
+		// Space Complexity: O(1)
+		if (str == null || str.isEmpty()) {
+			return 0;
+		}
+		int wordCount = 0;
+		boolean isWord = false;
+		int endOfLine = str.length() - 1;
+
+		for (int i = 0; i < str.length(); i++) {
+			if (Character.isLetter(str.charAt(i)) && i != endOfLine) {
+				isWord = true;
+			} else if (!Character.isLetter(str.charAt(i)) && isWord) {
+				wordCount++;
+				isWord = false;
+			} else if (Character.isLetter(str.charAt(i)) && i == endOfLine) {
+				wordCount++;
+			}
+		}
+		return wordCount;
+	}
 }
