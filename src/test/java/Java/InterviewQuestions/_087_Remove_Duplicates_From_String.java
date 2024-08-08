4780adf8c2b9790f33afe2bea6ce713348a5677d
avdhutssh <avdhut.ssh@gmail.com>
2024-08-08 09:53:38 +0530

    Remove duplicates from a string using boolean array

--- src/test/java/Java/InterviewQuestions/_087_Remove_Duplicates_From_String.java
+++ src/test/java/Java/InterviewQuestions/_087_Remove_Duplicates_From_String.java
@@ -7,11 +7,13 @@ public class _087_Remove_Duplicates_From_String {
 	public static void main(String[] args) {
 		String str = "HappyNewYear"; // HapyNewYr
 
-		System.out.println(_01_Using_HashSet(str));
+		System.out.println("Using HashSet: " + _01_Using_HashSet(str));
+		System.out.println("Using Arrays: " + _02_Using_Array(str));
 	}
 
 	private static String _01_Using_HashSet(String str) {
-
+		// Time Complexity: O(n)
+		// Space Complexity: O(n)
 		HashSet<Character> hs = new HashSet<>();
 		String result = "";
 		for (char ch : str.toCharArray()) {
@@ -19,6 +21,20 @@ public class _087_Remove_Duplicates_From_String {
 				result += ch;
 		}
 		return result;
+	}
+
+	private static String _02_Using_Array(String str) {
+		// Time Complexity: O(n)
+		// Space Complexity: O(n)
+		boolean[] exists = new boolean[256];
+		StringBuilder sb = new StringBuilder();
+		for (char ch : str.toCharArray()) {
+			if (!exists[ch]) {
+				exists[ch] = true;
+				sb.append(ch);
+			}
+		}
+		return sb.toString();
 	}
 
 }
