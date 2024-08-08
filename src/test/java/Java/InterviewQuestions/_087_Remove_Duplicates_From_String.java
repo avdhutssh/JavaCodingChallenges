0df692796bde83b1c6ecb7d29b262c4a358f979e
avdhutssh <avdhut.ssh@gmail.com>
2024-08-08 10:25:31 +0530

    Remove duplicates from a string using BFA

--- src/test/java/Java/InterviewQuestions/_087_Remove_Duplicates_From_String.java
+++ src/test/java/Java/InterviewQuestions/_087_Remove_Duplicates_From_String.java
@@ -1,18 +1,41 @@
 package Java.InterviewQuestions;
 
+import java.util.Arrays;
 import java.util.HashSet;
 
 public class _087_Remove_Duplicates_From_String {
 
 	public static void main(String[] args) {
-		String str = "HappyNewYear"; // HapyNewYr
+		String str1 = "HappyNewYear"; // HapyNewYr
+		String str2 = "HappyNewYear"; // HapyNewYr
 
-		System.out.println("Using HashSet: " + _01_Using_HashSet(str));
-		System.out.println("Using Arrays: " + _02_Using_Array(str));
-		System.out.println("Using StringBuilder: " + _03_Using_StringBuilder(str));
+		System.out.println("Using BFA: " + _01_Using_BruteForceApproach(str1.toCharArray()));
+		System.out.println("Using HashSet: " + _02_Using_HashSet(str2));
+		System.out.println("Using Arrays: " + _03_Using_Array(str2));
+		System.out.println("Using StringBuilder: " + _04_Using_StringBuilder(str2));
 	}
 
-	private static String _01_Using_HashSet(String str) {
+	private static String _01_Using_BruteForceApproach(char[] str) {
+		// Time Complexity: O(n^2)
+		// Space Complexity: O(n)a
+		int index = 0;
+		for (int i = 0; i < str.length; i++) {
+			int j;
+			for (j = 0; j < i; j++) {
+				if (str[i] == str[j]) {
+					break;
+				}
+			}
+			if (j == i) {
+				str[index++] = str[i];
+			}
+
+		}
+
+		return String.valueOf(Arrays.copyOf(str, index));
+	}
+
+	private static String _02_Using_HashSet(String str) {
 		// Time Complexity: O(n)
 		// Space Complexity: O(n)
 		HashSet<Character> hs = new HashSet<>();
@@ -24,7 +47,7 @@ public class _087_Remove_Duplicates_From_String {
 		return result;
 	}
 
-	private static String _02_Using_Array(String str) {
+	private static String _03_Using_Array(String str) {
 		// Time Complexity: O(n)
 		// Space Complexity: O(n)
 		boolean[] exists = new boolean[256];
@@ -38,7 +61,7 @@ public class _087_Remove_Duplicates_From_String {
 		return sb.toString();
 	}
 
-	private static String _03_Using_StringBuilder(String str) {
+	private static String _04_Using_StringBuilder(String str) {
 		// Time Complexity: O(n^2)
 		// Space Complexity: O(n)
 		StringBuilder sb = new StringBuilder();
