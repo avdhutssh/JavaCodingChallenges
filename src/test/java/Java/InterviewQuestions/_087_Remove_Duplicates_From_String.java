fa444ad59c5ee1541d4d8b686fc5c79d081459d0
avdhutssh <avdhut.ssh@gmail.com>
2024-08-08 10:07:51 +0530

    Remove duplicates from a string using String builder and substring

--- src/test/java/Java/InterviewQuestions/_087_Remove_Duplicates_From_String.java
+++ src/test/java/Java/InterviewQuestions/_087_Remove_Duplicates_From_String.java
@@ -9,6 +9,7 @@ public class _087_Remove_Duplicates_From_String {
 
 		System.out.println("Using HashSet: " + _01_Using_HashSet(str));
 		System.out.println("Using Arrays: " + _02_Using_Array(str));
+		System.out.println("Using StringBuilder: " + _03_Using_StringBuilder(str));
 	}
 
 	private static String _01_Using_HashSet(String str) {
@@ -32,6 +33,19 @@ public class _087_Remove_Duplicates_From_String {
 			if (!exists[ch]) {
 				exists[ch] = true;
 				sb.append(ch);
+			}
+		}
+		return sb.toString();
+	}
+
+	private static String _03_Using_StringBuilder(String str) {
+		// Time Complexity: O(n^2)
+		// Space Complexity: O(n)
+		StringBuilder sb = new StringBuilder();
+		for (int i = 0; i < str.length(); i++) {
+			String subStr = str.substring(0, i);
+			if (!subStr.contains(String.valueOf(str.charAt(i)))) {
+				sb.append(str.charAt(i));
 			}
 		}
 		return sb.toString();
