daadbc66e4f4376bb87244c9e3bea492b840b1b6
avdhutssh <avdhut.ssh@gmail.com>
2024-08-10 01:39:11 +0530

    Count letters in String Using TreeMap

--- src/test/java/Java/InterviewQuestions/_088_Count_letters.java
+++ src/test/java/Java/InterviewQuestions/_088_Count_letters.java
@@ -1,18 +1,34 @@
 package Java.InterviewQuestions;
 
 import java.util.HashMap;
+import java.util.Map;
+import java.util.TreeMap;
 
 public class _088_Count_letters {
 
 	public static void main(String[] args) {
 		String str = "GeeksForGeeks"; // r 1 s 2 e 4 F 1 G 2 k 2 o 1
-		System.out.println(_01_Using_Map(str));
+		System.out.println("Using HashMap: " + _01_Using_HashMap(str));
+		System.out.println("Using TreeMap: " + _02_Using_TreeMap(str));
 	}
 
-	private static HashMap<Character, Integer> _01_Using_Map(String str) {
+	private static HashMap<Character, Integer> _01_Using_HashMap(String str) {
+		// Time complexity: O(nlogk) //k = number of unique elements
+		// Space complexity: O(k)
+		HashMap<Character, Integer> hs = new HashMap<>();
+		for (char ch : str.toCharArray()) {
+			if (Character.isLetter(ch)) {
+				ch = Character.toLowerCase(ch);
+				hs.put(ch, hs.getOrDefault(ch, 0) + 1);
+			}
+		}
+		return hs;
+	}
+
+	private static Map<Character, Integer> _02_Using_TreeMap(String str) {
 		// Time complexity: O(n)
 		// Space complexity: O(1)
-		HashMap<Character, Integer> hs = new HashMap<>();
+		Map<Character, Integer> hs = new TreeMap<>();
 		for (char ch : str.toCharArray()) {
 			if (Character.isLetter(ch)) {
 				ch = Character.toLowerCase(ch);
