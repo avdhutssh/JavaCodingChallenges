221a8a94cf82add11fa72ff6b63426e170c233c5
avdhutssh <avdhut.ssh@gmail.com>
2024-08-12 11:48:22 +0530

    FizzBuzz Using String Concatenation

--- src/test/java/Java/InterviewQuestions/_089_FizzBuzz.java
+++ src/test/java/Java/InterviewQuestions/_089_FizzBuzz.java
@@ -3,11 +3,12 @@ package Java.InterviewQuestions;
 public class _089_FizzBuzz {
 
 	public static void main(String[] args) {
-		// iber/ 3 print Fizz iber/ 5 print Buzz iber/ both 3 and 5 print FizzBuzz
+// number/ 3 print Fizz number/ 5 print Buzz number/ both 3 and 5 print FizzBuzz else print number
 
-		int num1 = 15; // 1 2 Fizz 4 Buzz Fizz 7 8 Fizz Buzz 11 Fizz 13 14 FizzBuzz
+		int num = 15; // 1 2 Fizz 4 Buzz Fizz 7 8 Fizz Buzz 11 Fizz 13 14 FizzBuzz
 
-		_01_Using_BruteForceApproach(num1);
+		_01_Using_BruteForceApproach(num);
+		_02_Using_StringConcatenation(num);
 
 	}
 
@@ -25,7 +26,26 @@ public class _089_FizzBuzz {
 				System.out.print(i + " ");
 			}
 		}
+		System.out.println();
 
+	}
+
+	private static void _02_Using_StringConcatenation(int num) {
+		// Time complexity: O(n)
+		// Space complexity: O(1)
+		String result = "";
+		for (int i = 1; i <= num; i++) {
+			if (i % 3 == 0 && i % 5 == 0) {
+				result += "FizzBuzz ";
+			} else if (i % 3 == 0) {
+				result += "Fizz ";
+			} else if (i % 5 == 0) {
+				result += "Buzz ";
+			} else {
+				result += String.valueOf(i+" ");
+			}
+		}
+		System.out.println(result);
 	}
 
 }
