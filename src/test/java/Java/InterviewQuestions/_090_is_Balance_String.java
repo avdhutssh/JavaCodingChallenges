b3b67c021cad74816d60c622913db29bccecdd07
avdhutssh <avdhut.ssh@gmail.com>
2024-08-13 11:22:42 +0530

    Balanced String using Recursion

--- src/test/java/Java/InterviewQuestions/_090_is_Balance_String.java
+++ src/test/java/Java/InterviewQuestions/_090_is_Balance_String.java
@@ -11,6 +11,13 @@ public class _090_is_Balance_String {
 		System.out.println(_01_Using_Stack("({}{}([{}]))")); // true
 		System.out.println(_01_Using_Stack("({")); // false
 		System.out.println(_01_Using_Stack("{[[(])]}")); // false
+		System.out.println("--------------------------------------------");
+		System.out.println(_02_Using_Recursion("[{()}]")); // true
+		System.out.println(_02_Using_Recursion("[({(})]")); // false
+		System.out.println(_02_Using_Recursion("{[}")); // false
+		System.out.println(_02_Using_Recursion("({}{}([{}]))")); // true
+		System.out.println(_02_Using_Recursion("({")); // false
+		System.out.println(_02_Using_Recursion("{[[(])]}")); // false
 	}
 
 	private static boolean _01_Using_Stack(String str) {
@@ -31,6 +38,19 @@ public class _090_is_Balance_String {
 			}
 		}
 		return st.isEmpty();
+	}
+
+	private static boolean _02_Using_Recursion(String str) {
+		// Time complexity: O(n^2)
+		// Space complexity: O(n)
+		if (str.length() == 0) {
+			return true;
+		}
+		String reducedStr = str.replaceAll("\\(\\)", "").replaceAll("\\{\\}", "").replaceAll("\\[\\]", "");
+		if (reducedStr.length() == str.length()) {
+			return false;
+		}
+		return _02_Using_Recursion(reducedStr);
 	}
 
 }
