cbd297c0f2c4de55f6fc8e39a081a6b58a730d5c
avdhutssh <avdhut.ssh@gmail.com>
2024-08-16 21:49:54 +0530

    palindrome substrings of a given string using Dynamic Programming

--- src/test/java/Java/InterviewQuestions/_091_Palindrome_Substrings.java
+++ src/test/java/Java/InterviewQuestions/_091_Palindrome_Substrings.java
@@ -6,6 +6,8 @@ public class _091_Palindrome_Substrings {
 
 		String str = "abaaa"; // a aa aaa aba b
 		_01_UsingBruteForceApproach(str);
+		System.out.println("\n"+"--------------------------------");
+		_02_Using_Dynamic_Programming(str);
 
 	}
 
@@ -17,7 +19,7 @@ public class _091_Palindrome_Substrings {
 			for (int j = i + 1; j < str.length(); j++) {
 				String subStr = str.substring(i, j);
 				if (isPalindrome(subStr)) {
-					System.out.print(subStr+" ");
+					System.out.print(subStr + " ");
 				}
 			}
 		}
@@ -39,4 +41,34 @@ public class _091_Palindrome_Substrings {
 
 	}
 
+	private static void _02_Using_Dynamic_Programming(String str) {
+		// Time complexity: O(n^2)
+		// Space complexity: O(n^2)
+		System.out.print("Using Dynamic Programming: ");
+		int n = str.length();
+		boolean[][] dp = new boolean[n][n];
+		for (int i = 0; i < n; i++) {
+			dp[i][i] = true;
+			System.out.print(str.charAt(i) + " ");
+		}
+
+		for (int i = 0; i < n-1; i++) {
+			if (str.charAt(i) == str.charAt(i + 1)) {
+				dp[i][i + 1] = true;
+				System.out.print(str.charAt(i) + "" + str.charAt(i + 1) + " ");
+			}
+		}
+
+		for (int length = 3; length <= n; length++) {
+			for (int i = 0; i <= n - length; i++) {
+				int j = i + length - 1;
+				if (str.charAt(i) == str.charAt(j) && dp[i + 1][j - 1]) {
+					dp[i][j] = true;
+					System.out.print(str.substring(i, j + 1) + " ");
+				}
+
+			}
+
+		}
+	}
 }
