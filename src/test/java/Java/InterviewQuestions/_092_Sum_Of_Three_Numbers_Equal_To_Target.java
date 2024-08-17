ed88e02cfea06227c19f1a2f3a965e495d6f589f
avdhutssh <avdhut.ssh@gmail.com>
2024-08-17 12:49:33 +0530

    sum of any three integers in an array is equal to the given value using two pointer Approach with sorting

--- src/test/java/Java/InterviewQuestions/_092_Sum_Of_Three_Numbers_Equal_To_Target.java
+++ src/test/java/Java/InterviewQuestions/_092_Sum_Of_Three_Numbers_Equal_To_Target.java
@@ -1,11 +1,14 @@
 package Java.InterviewQuestions;
 
+import java.util.Arrays;
+
 public class _092_Sum_Of_Three_Numbers_Equal_To_Target {
 
 	public static void main(String[] args) {
-		int[] arr = { 1, 2, 4, 5, 12 };
+		int[] arr = { 1, 2, 4, 5, 15, 12 };
 		int target = 21;
 		_01_Using_BruteForceApproach(arr, target);
+		_02_Using_TwoPointerApproach_Sorting(arr, target);
 
 	}
 
@@ -25,4 +28,27 @@ public class _092_Sum_Of_Three_Numbers_Equal_To_Target {
 		}
 	}
 
+	private static void _02_Using_TwoPointerApproach_Sorting(int[] arr, int target) {
+		// Time complexity: O(n^2)
+		// Space complexity: O(1)
+		System.out.println("Using Two Pointer Approach with Sorting: ");
+		Arrays.sort(arr);
+		for (int i = 0; i < arr.length; i++) {
+			int left = i + 1;
+			int right = arr.length - 1;
+			while (left < right) {
+				int sum = arr[i] + arr[left] + arr[right];
+				if (sum == target) {
+					System.out.println(arr[i] + " " + arr[left] + " " + arr[right]);
+					left++;
+					right--;
+				} else if (sum < target) {
+					left++;
+				} else {
+					right--;
+				}
+			}
+
+		}
+	}
 }
