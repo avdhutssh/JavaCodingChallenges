070ab53bda17bbd913d888059807276bc39c4036
avdhutssh <avdhut.ssh@gmail.com>
2024-09-06 10:25:39 +0530

    Sorting Sentence Using HashMap

--- src/test/java/Java/InterviewQuestions/_100_Sorting_Sentence.java
+++ src/test/java/Java/InterviewQuestions/_100_Sorting_Sentence.java
@@ -1,5 +1,7 @@
 package Java.InterviewQuestions;
 
+import java.util.HashMap;
+
 public class _100_Sorting_Sentence {
 
 	public static void main(String[] args) {
@@ -8,9 +10,12 @@ public class _100_Sorting_Sentence {
 
 		System.out.println("Using BFA: " + _01_Using_BruteForceApproach(str1));
 		System.out.println("Using BFA: " + _01_Using_BruteForceApproach(str2));
-
+		System.out.println("--------------------------------------------------");
 		System.out.println("Using Optimized Approach: " + _02_Using_OptimizedApproach(str1));
 		System.out.println("Using Optimized Approach: " + _02_Using_OptimizedApproach(str2));
+		System.out.println("--------------------------------------------------");
+		System.out.println("Using HashMap: " + _03_Using_HashMap(str1));
+		System.out.println("Using HashMap: " + _03_Using_HashMap(str2));
 
 	}
 
@@ -43,4 +48,19 @@ public class _100_Sorting_Sentence {
 		return String.join(" ", bucket);
 	}
 
+	private static String _03_Using_HashMap(String str) {
+		// Time complexity: O(n)
+		// Space complexity: O(n)
+		String[] words = str.split("\\s+");
+		HashMap<Integer, String> hs = new HashMap<>();
+		for (String word : words) {
+			int index = Character.getNumericValue(word.charAt(word.length() - 1));
+			hs.put(index, word.substring(0, word.length() - 1));
+		}
+		StringBuilder sb = new StringBuilder();
+		for (int i = 1; i <= words.length; i++) {
+			sb.append(hs.get(i)).append(" ");
+		}
+		return sb.toString().trim();
+	}
 }
