ada0fd843018e875e7935f8d14759b48a17041a2
avdhutssh <avdhut.ssh@gmail.com>
2024-09-06 09:13:01 +0530

    Sorting Sentence Using BFA

---
+++ src/test/java/Java/InterviewQuestions/_100_Sorting_Sentence.java
@@ -0,0 +1,28 @@
+package Java.InterviewQuestions;
+
+public class _100_Sorting_Sentence {
+
+	public static void main(String[] args) {
+		String str1 = "is2 sentence4 This1 a3"; // "This is a sentence"
+		String str2 = "Myself2 Me1 I4 and3"; // "Me Myself and I"
+
+		System.out.println("Using BFA: " + _01_Using_BruteForceApproach(str1));
+		System.out.println("Using BFA: " + _01_Using_BruteForceApproach(str2));
+
+	}
+
+	private static String _01_Using_BruteForceApproach(String str) {
+		String result = "";
+		String[] strArr = str.split("\\s+");
+		String[] resultArr = new String[strArr.length];
+		for (String s : strArr) {
+			int index = (int) (s.charAt(s.length() - 1) - '0');
+			resultArr[index - 1] = s.substring(0, s.length() - 1);
+		}
+		for (String s : resultArr) {
+			result += s + " ";
+		}
+		return result;
+	}
+
+}
