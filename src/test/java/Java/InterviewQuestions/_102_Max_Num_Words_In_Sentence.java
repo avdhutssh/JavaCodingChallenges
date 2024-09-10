74b05973ed553655fbff9907cd4d1a6b4b4c865c
avdhutssh <avdhut.ssh@gmail.com>
2024-09-10 16:01:34 +0530

    Maximum Number of Words in Sentences Using BFA

--- src/test/java/Java/InterviewQuestions/_102_Max_Num_Words_In_Sentence.java
+++ src/test/java/Java/InterviewQuestions/_102_Max_Num_Words_In_Sentence.java
@@ -1,3 +1,25 @@
+package Java.InterviewQuestions;
+
 public class _102_Max_Num_Words_In_Sentence {
-    
+	public static void main(String[] args) {
+		String[] sentences1 = { "alice and bob love leetcode", "i think so too", "this is great thanks very much" }; // 6
+		String[] sentences2 = { "please wait", "continue to fight", "continue to win" }; // 3
+
+		System.out.println("Using BFA: " + _01_Using_BruteForceApproach(sentences1));
+		System.out.println("Using BFA: " + _01_Using_BruteForceApproach(sentences2));
+		System.out.println("--------------------------------------------------");
+
+	}
+
+	private static int _01_Using_BruteForceApproach(String[] sentences) {
+		// Time complexity: O(n * m)
+		// Space complexity: O(1)
+		int max = 0;
+		for (String str : sentences) {
+			String[] sentence = str.split("\\s+");
+			max = Math.max(max, sentence.length);
+		}
+		return max;
+	}
+
 }
