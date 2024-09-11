904d71aca4c0a85644556d7ffda62b31f0d0a5df
avdhutssh <avdhut.ssh@gmail.com>
2024-09-11 09:50:52 +0530

    Maximum Number of Words in Sentences Using Streams

--- src/test/java/Java/InterviewQuestions/_102_Max_Num_Words_In_Sentence.java
+++ src/test/java/Java/InterviewQuestions/_102_Max_Num_Words_In_Sentence.java
@@ -1,5 +1,7 @@
 package Java.InterviewQuestions;
 
+import java.util.Arrays;
+
 public class _102_Max_Num_Words_In_Sentence {
 	public static void main(String[] args) {
 		String[] sentences1 = { "alice and bob love leetcode", "i think so too", "this is great thanks very much" }; // 6
@@ -7,6 +9,9 @@ public class _102_Max_Num_Words_In_Sentence {
 
 		System.out.println("Using BFA: " + _01_Using_BruteForceApproach(sentences1));
 		System.out.println("Using BFA: " + _01_Using_BruteForceApproach(sentences2));
+		System.out.println("--------------------------------------------------");
+		System.out.println("Using Streams: " + _02_Using_Streams(sentences1));
+		System.out.println("Using Streams: " + _02_Using_Streams(sentences2));
 		System.out.println("--------------------------------------------------");
 
 	}
@@ -22,4 +27,9 @@ public class _102_Max_Num_Words_In_Sentence {
 		return max;
 	}
 
+	private static int _02_Using_Streams(String[] sentences) {
+		// Time complexity: O(n * m)
+		// Space complexity: O(n)
+		return Arrays.stream(sentences).mapToInt(sentence -> sentence.split("\\s+").length).max().orElse(0);
+	}
 }
