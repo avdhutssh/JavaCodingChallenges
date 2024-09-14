176dad01a849a117bd229c0d3c2f7b95d3f7cbf6
avdhutssh <avdhut.ssh@gmail.com>
2024-09-14 21:42:31 +0530

    swap the first and last letter in a string using BFA

--- src/test/java/Java/InterviewQuestions/_103_Swap_First_Last_Letter_String.java
+++ src/test/java/Java/InterviewQuestions/_103_Swap_First_Last_Letter_String.java
@@ -1,10 +1,31 @@
 package Java.InterviewQuestions;
 
+import java.util.Arrays;
+
 public class _103_Swap_First_Last_Letter_String {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		String str = "That is a sample"; // thaT si a eampls
+		System.out.println("Using BFA: " + _01_Using_BruteForceApproach(str));
 
+	}
+
+	private static String _01_Using_BruteForceApproach(String str) {
+		// Time complexity: O(n)
+		// Space complexity: O(n)
+		String[] words = str.split("\\s+");
+		for (int i = 0; i < words.length; i++) {
+			String first = words[i].substring(0, 1);
+			String last = words[i].substring(words[i].length() - 1);
+			if (words[i].length() == 1) {
+				words[i] = first;
+			} else if (words[i].length() <= 2) {
+				words[i] = last + first;
+			} else {
+				words[i] = last + words[i].substring(1, words[i].length() - 1) + first;
+			}
+		}
+		return Arrays.toString(words);
 	}
 
 }
