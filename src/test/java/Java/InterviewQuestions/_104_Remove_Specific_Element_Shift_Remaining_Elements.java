bca562138f458c6f57b029b867458415bf08a8ac
avdhutssh <avdhut.ssh@gmail.com>
2024-09-19 09:57:10 +0530

    remove a specific element from an array and shift the remaining elements using List

--- src/test/java/Java/InterviewQuestions/_104_Remove_Specific_Element_Shift_Remaining_Elements.java
+++ src/test/java/Java/InterviewQuestions/_104_Remove_Specific_Element_Shift_Remaining_Elements.java
@@ -1,26 +1,44 @@
 package Java.InterviewQuestions;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class _104_Remove_Specific_Element_Shift_Remaining_Elements {
 
 	public static void main(String[] args) {
-		int[] arr = { 1, 2, 3, 4, 5 }; //element = 3, Output: arr[] = { 1, 2, 4, 5 }
-		System.out.println("Using BFA: " + Arrays.toString(_01_Using_BruteForceApproach(arr,3)));
-				
+		int[] arr = { 1, 2, 3, 4, 5 }; // element = 3, Output: arr[] = { 1, 2, 4, 5 }
+		System.out.println("Using BFA: " + Arrays.toString(_01_Using_BruteForceApproach(arr, 3)));
+		System.out.println("Using List: " + Arrays.toString(_02_Using_List(arr, 3)));
 
 	}
 
-	private static int[] _01_Using_BruteForceApproach(int[] arr,int element) {
+	private static int[] _01_Using_BruteForceApproach(int[] arr, int element) {
 		// Time complexity: O(n)
 		// Space complexity: O(n)
-		int[] result = new int[arr.length-1];
-		int j =0;
+		int[] result = new int[arr.length - 1];
+		int j = 0;
 		for (int i = 0; i < arr.length; i++) {
-			if(arr[i]!=element) {
+			if (arr[i] != element) {
 				result[j++] = arr[i];
 			}
-			
+
+		}
+		return result;
+	}
+
+	private static int[] _02_Using_List(int[] arr, int element) {
+		// Time complexity: O(n)
+		// Space complexity: O(n)
+		List<Integer> al = new ArrayList<>();
+		for (int i = 0; i < arr.length; i++) {
+			if (arr[i] != element) {
+				al.add(arr[i]);
+			}
+		}
+		int[] result = new int[al.size()];
+		for (int i = 0; i < al.size(); i++) {
+			result[i] = al.get(i);
 		}
 		return result;
 	}
