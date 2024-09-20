d97ab65da6da72beed356802315325613e2adf00
avdhutssh <avdhut.ssh@gmail.com>
2024-09-20 09:31:37 +0530

    remove a specific element from an array and shift the remaining elements using InPlace Removal and Stream

--- src/test/java/Java/InterviewQuestions/_104_Remove_Specific_Element_Shift_Remaining_Elements.java
+++ src/test/java/Java/InterviewQuestions/_104_Remove_Specific_Element_Shift_Remaining_Elements.java
@@ -10,6 +10,8 @@ public class _104_Remove_Specific_Element_Shift_Remaining_Elements {
 		int[] arr = { 1, 2, 3, 4, 5 }; // element = 3, Output: arr[] = { 1, 2, 4, 5 }
 		System.out.println("Using BFA: " + Arrays.toString(_01_Using_BruteForceApproach(arr, 3)));
 		System.out.println("Using List: " + Arrays.toString(_02_Using_List(arr, 3)));
+		System.out.print("Using InPlace Removal and Stream: ");
+		Arrays.stream(arr, 0, _03_Using_InPlaceRemoval(arr, 3)).forEach(e -> System.out.print(e + " "));
 
 	}
 
@@ -41,6 +43,19 @@ public class _104_Remove_Specific_Element_Shift_Remaining_Elements {
 			result[i] = al.get(i);
 		}
 		return result;
+	}
+
+	private static int _03_Using_InPlaceRemoval(int[] arr, int element) {
+		// Time complexity: O(n)
+		// Space complexity: O(1)
+		int index = 0;
+		for (int i = 0; i < arr.length; i++) {
+			if (arr[i] != element) {
+				arr[index++] = arr[i];
+			}
+
+		}
+		return index;
 	}
 
 }
