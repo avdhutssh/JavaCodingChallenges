0841f53c342ad3045a6523c80acfab44b39f55f3
avdhutssh <avdhut.ssh@gmail.com>
2024-09-24 18:46:39 +0530

    is a valid email address using RegX

--- src/test/java/Java/InterviewQuestions/_106_Is_Valid_Email.java
+++ src/test/java/Java/InterviewQuestions/_106_Is_Valid_Email.java
@@ -1,5 +1,8 @@
 package Java.InterviewQuestions;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class _106_Is_Valid_Email {
 	public static void main(String[] args) {
 		String validMail = "avdhut1234@gmail.com";
@@ -10,11 +13,18 @@ public class _106_Is_Valid_Email {
 		System.out.println("Using BFA: " + _01_Using_BruteForceApproach(invalidMail1));
 		System.out.println("Using BFA: " + _01_Using_BruteForceApproach(invalidMail2));
 		System.out.println("Using BFA: " + _01_Using_BruteForceApproach(invalidMail3));
+		
+		
+		System.out.println("Using RegX: " + isValidEmail(validMail));
+		System.out.println("Using RegX: " + isValidEmail(invalidMail1));
+		System.out.println("Using RegX: " + isValidEmail(invalidMail2));
+		System.out.println("Using RegX: " + isValidEmail(invalidMail3));
 
 	}
 
 	private static boolean _01_Using_BruteForceApproach(String str) {
-
+		// Time complexity: O(n)
+		// Space complexity: O(1)
 		if (!str.contains(".com")) {
 			return false;
 		}
@@ -26,4 +36,15 @@ public class _106_Is_Valid_Email {
 		}
 		return true;
 	}
+	
+    
+
+    public static boolean isValidEmail(String email) {
+		// Time complexity: O(n)
+		// Space complexity: O(1)
+    	final String EMAIL_REGEX = "^[a-zA-Z0-9_+&*-]+(?:\\.[a-zA-Z0-9_+&*-]+)*@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,7}$";
+        Pattern pattern = Pattern.compile(EMAIL_REGEX);
+        Matcher matcher = pattern.matcher(email);
+        return matcher.matches();
+    }
 }
