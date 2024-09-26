0181b9a5730402fcf0c51673a636a4ec50a61e50
avdhutssh <avdhut.ssh@gmail.com>
2024-09-26 09:40:32 +0530

    convert the first letter of each word in a string to uppercase using Stream

--- src/test/java/Java/InterviewQuestions/_107_Uppercase_First_Letter_Of_Each_Word.java
+++ src/test/java/Java/InterviewQuestions/_107_Uppercase_First_Letter_Of_Each_Word.java
@@ -1,11 +1,16 @@
 package Java.InterviewQuestions;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.stream.Collectors;
+
 public class _107_Uppercase_First_Letter_Of_Each_Word {
 
 	public static void main(String[] args) {
 
-		String str1 = "avdhut is a great person";
-		System.out.println("Using BFA: " + _01_Using_BruteForceApproach(str1));
+		String str = "avdhut is a great person";
+		System.out.println("Using BFA: " + _01_Using_BruteForceApproach(str));
+		System.out.println("Using Stream: " + _02_Using_Stream(str));
 
 	}
 
@@ -18,6 +23,14 @@ public class _107_Uppercase_First_Letter_Of_Each_Word {
 			result.append(Character.toUpperCase(word.charAt(0))).append(word.substring(1).toLowerCase()).append(" ");
 		}
 		return result.toString();
+	}
+
+	private static String _02_Using_Stream(String str) {
+		// Time complexity: O(n)
+		// Space complexity: O(n)
+		return Arrays.stream(str.split("\\s+"))
+				.map(word -> word.substring(0, 1).toUpperCase() + word.substring(1).toLowerCase())
+				.collect(Collectors.joining(" "));
 	}
 
 }
