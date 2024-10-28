7178a9166d5e8cc01ed5247dfb2c79b3df338596
avdhutssh <avdhut.ssh@gmail.com>
2024-10-28 21:35:05 +0530

    _114_mostFreqentNumberInArray

---
+++ src/test/java/Java/InterviewQuestions/_114_mostFreqentNumberInArray.java
@@ -0,0 +1,43 @@
+package Java.InterviewQuestions;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class _114_mostFreqentNumberInArray {
+	public static void main(String[] args) {
+		int[] array1 = { 1, 3, 1, 3, 2, 1 }; // 1
+		int[] array2 = { 3, 3, 1, 3, 2, 1 }; // 3
+		int[] array3 = {}; // null
+		int[] array4 = { 0 }; // 0
+		int[] array5 = { 0, -1, 10, 10, -1, 10, -1, -1, -1, 1 }; // -1
+		System.out.println("arr1: " + _01_Using_HashMap(array1));
+		System.out.println("arr2: " + _01_Using_HashMap(array2));
+		System.out.println("arr3: " + _01_Using_HashMap(array3));
+		System.out.println("arr4: " + _01_Using_HashMap(array4));
+		System.out.println("arr5: " + _01_Using_HashMap(array5));
+
+	}
+
+	public static int _01_Using_HashMap(int[] givenArray) {
+		// Time Complexity: O(n)
+		// Space Complexity: O(n)
+
+		if (givenArray.length == 0) {
+			return 0;
+		}
+		HashMap<Integer, Integer> hm = new HashMap<>();
+		for (int num : givenArray) {
+			hm.put(num, hm.getOrDefault(num, 0) + 1);
+		}
+		Integer maxItem = null;
+		int maxfrequency = Integer.MIN_VALUE;
+		for (int key : hm.keySet()) {
+			int frequency = hm.get(key);
+			if (frequency > maxfrequency) {
+				maxfrequency = frequency;
+				maxItem = key;
+			}
+		}
+		return maxItem;
+	}
+}
