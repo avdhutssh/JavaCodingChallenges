b4cf5a4fd6c636ce33c24b00a69a155e75efd212
avdhutssh <avdhut.ssh@gmail.com>
2024-11-19 21:07:22 +0530

    Reverse_String_And_ReplaceSpaceWithPerc_While_Preserving_Words_Order Optimized Solution

--- src/test/java/Java/InterviewQuestions/_126_Reverse_String_And_ReplaceSpaceWithPerc_While_Preserving_Words_Order.java
+++ src/test/java/Java/InterviewQuestions/_126_Reverse_String_And_ReplaceSpaceWithPerc_While_Preserving_Words_Order.java
@@ -7,6 +7,7 @@ public class _126_Reverse_String_And_ReplaceSpaceWithPerc_While_Preserving_Words
 		String str1 = "India is my country"; // aidnI%si%ym%yrtnuoc
 
 		System.out.println(_01_UsingBruteForceApproach(str1));
+		System.out.println(_02_OptimizedSolution(str1));
 
 	}
 
@@ -17,7 +18,7 @@ public class _126_Reverse_String_And_ReplaceSpaceWithPerc_While_Preserving_Words
 		String[] words = str.split(" ");
 		for (int i = 0; i < words.length; i++) {
 			sb.append(reverseString(words[i]));
-			if (i != words.length-1) {
+			if (i != words.length - 1) {
 				sb.append("%");
 			}
 		}
@@ -35,4 +36,24 @@ public class _126_Reverse_String_And_ReplaceSpaceWithPerc_While_Preserving_Words
 		return sb.toString();
 	}
 
+	public static String _02_OptimizedSolution(String str) {
+		// Time Complexity: O(n)
+		// Space Complexity: O(n)
+
+		StringBuilder sb = new StringBuilder();
+		int start = 0;
+		for (int i = 0; i <= str.length(); i++) {
+			if (i == str.length() || str.charAt(i) == ' ') {
+				for (int j = i - 1; j >= start; j--) {
+					sb.append(str.charAt(j));
+				}
+				if (i != str.length()) {
+					sb.append("%");
+				}
+				start = i + 1;
+			}
+		}
+		return sb.toString();
+
+	}
 }
