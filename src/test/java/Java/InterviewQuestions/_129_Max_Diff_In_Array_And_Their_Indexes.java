874644df53b6063370dd13089eb8e9483613e993
avdhutssh <avdhut.ssh@gmail.com>
2024-11-27 09:46:04 +0530

    Max_Diff_In_Array_And_Their_Indexes Using Optimized Approach

--- src/test/java/Java/InterviewQuestions/_129_Max_Diff_In_Array_And_Their_Indexes.java
+++ src/test/java/Java/InterviewQuestions/_129_Max_Diff_In_Array_And_Their_Indexes.java
@@ -6,6 +6,7 @@ public class _129_Max_Diff_In_Array_And_Their_Indexes {
 
 		int[] arr1 = { 6, 1, 3, 7, 8, 4, 1 }; // diff =7 and index[1,4] where (j>i)
 		_01_UsingBruteForceApproach(arr1);
+		_02_UsingOptimizedApproach(arr1);
 
 	}
 
@@ -13,6 +14,7 @@ public class _129_Max_Diff_In_Array_And_Their_Indexes {
 		// Time Complexity: O(n^2)
 		// Space Complexity: O(1)
 
+		System.out.println("Using Brute Force Approach: ");
 		int maxDiff = Integer.MIN_VALUE;
 		int indexI = -1;
 		int indexJ = -1;
@@ -26,6 +28,36 @@ public class _129_Max_Diff_In_Array_And_Their_Indexes {
 					indexJ = j;
 				}
 			}
+		}
+
+		System.out.println("Difference: " + maxDiff);
+		System.out.println("Index: [" + indexI + ", " + indexJ + "]");
+
+//        return new int[] {maxDiff, indexI, indexJ};
+	}
+
+	private static void _02_UsingOptimizedApproach(int[] arr) {
+		// Time Complexity: O(n)
+		// Space Complexity: O(1)
+
+		System.out.println("Using Optimized Approach: ");
+
+		int maxDiff = Integer.MIN_VALUE;
+		int minElement = arr[0];
+		int indexI = -1;
+		int indexJ = -1;
+
+		for (int i = 0; i < arr.length - 1; i++) {
+			int diff = arr[i] - minElement;
+			if (diff > maxDiff) {
+				maxDiff = diff;
+				indexJ = i;
+			}
+			if (arr[i] < minElement) {
+				minElement = arr[i];
+				indexI = i;
+			}
+
 		}
 
 		System.out.println("Difference: " + maxDiff);
