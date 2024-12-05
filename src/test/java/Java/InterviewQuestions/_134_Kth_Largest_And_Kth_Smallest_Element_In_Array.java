3b9b7c89b280a79902cdf38d15b0366635819ec2
avdhutssh <avdhut.ssh@gmail.com>
2024-12-05 23:44:40 +0530

    Kth largest and Kth smallest element in array by sorting the array using maxHeap

--- src/test/java/Java/InterviewQuestions/_134_Kth_Largest_And_Kth_Smallest_Element_In_Array.java
+++ src/test/java/Java/InterviewQuestions/_134_Kth_Largest_And_Kth_Smallest_Element_In_Array.java
@@ -1,6 +1,8 @@
 package Java.InterviewQuestions;
 
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.PriorityQueue;
 
 public class _134_Kth_Largest_And_Kth_Smallest_Element_In_Array {
 
@@ -12,15 +14,41 @@ public class _134_Kth_Largest_And_Kth_Smallest_Element_In_Array {
 
 		_01_By_Using_InBuilt_Sorting_Array(arr1, K);
 		_01_By_Using_InBuilt_Sorting_Array(arr3, K);
+		
+		System.out.println("----------------------------");
+		
+		_01_By_Using_InBuilt_Sorting_Array(arr2, K);
+		_01_By_Using_InBuilt_Sorting_Array(arr3, K);
 	}
 
 	private static void _01_By_Using_InBuilt_Sorting_Array(int[] arr, int K) {
 		// Time complexity: O(nlog(n))
 		// Space complexity: O(1)
-
 		if (arr.length < K) {
 			System.out.println("Array size is less than K");
 			return;
+		}
+		
+		Arrays.sort(arr);
+		System.out.println("Kth Smallest: " + arr[K] + "\nKth Largest: " + arr[arr.length - K]);
+	}
+
+	private static void _02_By_Using_Heap_Sorting(int[] arr, int K) {
+		// Time complexity: O(nlog(n))
+		// Space complexity: O(1)
+		if (arr.length < K) {
+			System.out.println("Array size is less than K");
+			return;
+		}
+		
+		PriorityQueue<Integer> maxHeap = new PriorityQueue<>(Collections.reverseOrder());
+		for(int num : arr) {
+			maxHeap.add(num);
+		}
+		
+		int index =arr.length-1;
+		while(!maxHeap.isEmpty()) {
+			arr[index--] = maxHeap.poll();
 		}
 		
 		Arrays.sort(arr);
