d3d9b677985d3588704c62cfc1f4caa1638d5028
avdhutssh <avdhut.ssh@gmail.com>
2024-12-07 12:07:06 +0530

    insert an element in array Using BFA

---
+++ src/test/java/Java/InterviewQuestions/_135_Insert_Element_At_Specific_Position_In_Array.java
@@ -0,0 +1,31 @@
+package Java.InterviewQuestions;
+
+import java.util.Arrays;
+
+public class _135_Insert_Element_At_Specific_Position_In_Array {
+
+	public static void main(String[] args) {
+		int[] arr = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 }; // [1, 2, 3, 4, 50, 5, 6, 7, 8, 9, 10]
+		int element = 50;
+		int position = 5;
+
+		System.out.println(Arrays.toString(_01_Using_BruteForceApproach(arr, position, element)));
+	}
+
+	private static int[] _01_Using_BruteForceApproach(int[] arr, int position, int element) {
+		// Time complexity: O(n)
+		// Space complexity: O(n)
+		int[] newArr = new int[arr.length + 1];
+		for (int i = 0; i < newArr.length; i++) {
+			if (i < position-1) {
+				newArr[i] = arr[i];
+			} else if (i == position-1) {
+				newArr[i] = element;
+			} else {
+				newArr[i] = arr[i - 1];
+			}
+		}
+		return newArr;
+	}
+
+}
