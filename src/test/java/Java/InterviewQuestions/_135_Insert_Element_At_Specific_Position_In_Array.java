1f2b11387bb4c26a8dc8e383b37073b8f285172e
avdhutssh <avdhut.ssh@gmail.com>
2024-12-08 09:09:50 +0530

    insert an element in array Using List

--- src/test/java/Java/InterviewQuestions/_135_Insert_Element_At_Specific_Position_In_Array.java
+++ src/test/java/Java/InterviewQuestions/_135_Insert_Element_At_Specific_Position_In_Array.java
@@ -1,6 +1,8 @@
 package Java.InterviewQuestions;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class _135_Insert_Element_At_Specific_Position_In_Array {
 
@@ -10,6 +12,7 @@ public class _135_Insert_Element_At_Specific_Position_In_Array {
 		int position = 5;
 
 		System.out.println(Arrays.toString(_01_Using_BruteForceApproach(arr, position, element)));
+		System.out.println(Arrays.toString(_02_Using_List(arr, position, element)));
 	}
 
 	private static int[] _01_Using_BruteForceApproach(int[] arr, int position, int element) {
@@ -17,15 +20,27 @@ public class _135_Insert_Element_At_Specific_Position_In_Array {
 		// Space complexity: O(n)
 		int[] newArr = new int[arr.length + 1];
 		for (int i = 0; i < newArr.length; i++) {
-			if (i < position-1) {
+			if (i < position - 1) {
 				newArr[i] = arr[i];
-			} else if (i == position-1) {
+			} else if (i == position - 1) {
 				newArr[i] = element;
 			} else {
 				newArr[i] = arr[i - 1];
 			}
 		}
 		return newArr;
+	}
+
+	private static Object[] _02_Using_List(int[] arr, int position, int element) {
+		// Time complexity: O(n)
+		// Space complexity: O(n)
+		List<Integer> list = new ArrayList<>();
+		for (int num : arr) {
+			list.add(num);
+		}
+
+		list.add(position - 1, element);
+		return list.toArray();
 	}
 
 }
