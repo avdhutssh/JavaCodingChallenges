d22faaa2dfde3cc43cbc0d01131ef45080f6c9b2
avdhutssh <avdhut.ssh@gmail.com>
2024-12-11 07:48:55 +0530

    find the length of the string using indexOutOfBound exception

--- src/test/java/Java/InterviewQuestions/_136_Length_Of_String_Without_Using_LengthMethod.java
+++ src/test/java/Java/InterviewQuestions/_136_Length_Of_String_Without_Using_LengthMethod.java
@@ -1,12 +1,16 @@
 package Java.InterviewQuestions;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class _136_Length_Of_String_Without_Using_LengthMethod {
 //How to find the length of the string without using length?
-	
+
 	public static void main(String[] args) {
 
 		String str = "Avdhut"; // 6
 		_01_Using_For_Each_Loop(str);
+		_02_Using_IndexOutOfBound(str);
 
 	}
 
@@ -17,7 +21,22 @@ public class _136_Length_Of_String_Without_Using_LengthMethod {
 		for (char ch : str.toCharArray()) {
 			index++;
 		}
-		System.out.println("Length of String is: " + index);
+		System.out.println("Length of String for each loop is: " + index);
 	}
+
+	private static void _02_Using_IndexOutOfBound(String str) {
+		// Time Complexity: O(n)
+		// Space Complexity: O(1)
+		int index = 0;
+		try {
+			while (true) {
+				char ch = str.charAt(index);
+				index++;
+			}
+		} catch (StringIndexOutOfBoundsException e) {
+		}
+		System.out.println("Length of String using IndexOutOfBound is: " + index);
+	}
+
 
 }
