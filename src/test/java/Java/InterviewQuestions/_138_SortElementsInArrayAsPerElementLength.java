e63f0387c430498b754d45743d0306f6b5aee73d
avdhutssh <avdhut.ssh@gmail.com>
2024-12-15 23:56:27 +0530

    SortElementsInArrayAsPerElementLength

---
+++ src/test/java/Java/InterviewQuestions/_138_SortElementsInArrayAsPerElementLength.java
@@ -0,0 +1,14 @@
+package Java.InterviewQuestions;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class _138_SortElementsInArrayAsPerElementLength {
+
+	public static void main(String[] args) {
+        List<String> names1 = new ArrayList<>(Arrays.asList("Jithin", "Arunima", "Akhil", "Matthew", "Benjamin"));
+//        Sample output: [Akhil, Jithin, Matthew, Arunima, Benjamin])
+	}
+
+}
