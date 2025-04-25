7e7bd7df05b5cfa2e06df20cd2012dec4c94feff
avdhutssh <avdhut.ssh@gmail.com>
2025-04-25 21:08:12 +0530

    _02_SingleNumber_Optimized_Using_BitManipulation

--- src/test/java/Java/Leetcode/_002_SingleNumber.java
+++ src/test/java/Java/Leetcode/_002_SingleNumber.java
@@ -12,6 +12,15 @@ public class _002_SingleNumber {
 		System.out.println(_01_SingleNumber_BFA(new int[] { 0, 1, 0 })); // Expected: 1
 		System.out.println(_01_SingleNumber_BFA(new int[] { -1, -1, 0 })); // Expected: 0
 
+		System.out.println("-----------------------------------");
+
+		System.out.println(_02_SingleNumber_Optimized_Using_BitManipulation(new int[] { 4, 1, 2, 1, 2 })); // Expected:
+																											// 4
+		System.out.println(_02_SingleNumber_Optimized_Using_BitManipulation(new int[] { 2, 2, 1 })); // Expected: 1
+		System.out.println(_02_SingleNumber_Optimized_Using_BitManipulation(new int[] { 1 })); // Expected: 1
+		System.out.println(_02_SingleNumber_Optimized_Using_BitManipulation(new int[] { 0, 1, 0 })); // Expected: 1
+		System.out.println(_02_SingleNumber_Optimized_Using_BitManipulation(new int[] { -1, -1, 0 })); // Expected: 0
+
 	}
 
 	private static int _01_SingleNumber_BFA(int[] nums) {
@@ -27,6 +36,17 @@ public class _002_SingleNumber {
 			}
 		}
 		return hs.iterator().next();
+	}
+
+	private static int _02_SingleNumber_Optimized_Using_BitManipulation(int[] nums) {
+		// Time complexity: O(n)
+		// Space complexity: O(1)
+
+		int singleNum = 0;
+		for (int num : nums) {
+			singleNum = singleNum ^ num;
+		}
+		return singleNum;
 	}
 
 }
