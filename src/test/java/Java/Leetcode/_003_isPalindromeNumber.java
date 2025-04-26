6a580a2fe995ff3be0574d3b9bd9695f1b1d967f
avdhutssh <avdhut.ssh@gmail.com>
2025-04-26 12:27:17 +0530

    _01_IsPalindrome_ByReversingHalf

---
+++ src/test/java/Java/Leetcode/_003_isPalindromeNumber.java
@@ -0,0 +1,31 @@
+package Java.Leetcode;
+
+public class _003_isPalindromeNumber {
+
+	public static void main(String[] args) {
+		System.out.println(_01_IsPalindrome_ByReversingHalf(121)); // true
+		System.out.println(_01_IsPalindrome_ByReversingHalf(-121)); // false
+		System.out.println(_01_IsPalindrome_ByReversingHalf(10)); // false
+		System.out.println(_01_IsPalindrome_ByReversingHalf(12321)); // true
+		System.out.println(_01_IsPalindrome_ByReversingHalf(0)); // true
+		
+		System.out.println("-----------------------------------");
+		
+		
+	}
+
+	private static boolean _01_IsPalindrome_ByReversingHalf(int x) {
+		// Time complexity: O(log x)
+		// Space complexity: O(1)
+		if (x <= 0 || (x != 0 && x % 10 == 0)) {
+			return false;
+		}
+		int reverseHalf = 0;
+		while (x > reverseHalf) {
+			reverseHalf = reverseHalf * 10 + x % 10;
+			x /= 10;
+		}
+		return (reverseHalf == x) || (reverseHalf / 10 == x);
+	}
+
+}
