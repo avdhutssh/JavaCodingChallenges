24721fdb058b20105fcda4dc376105cdf3ba82da
avdhutssh <avdhut.ssh@gmail.com>
2025-04-27 21:55:07 +0530

    BestTimeToBuySellStocks using TPA

--- src/test/java/Java/Leetcode/_009_BestTimeToBuySellStocks.java
+++ src/test/java/Java/Leetcode/_009_BestTimeToBuySellStocks.java
@@ -14,16 +14,26 @@ public class _009_BestTimeToBuySellStocks {
 		int[] arr6 = {};
 		int[] arr7 = { 3, 3, 5, 0, 0, 3, 1, 4 };
 
-		System.out.println(maxProfit(arr1)); // Expected: 5
-		System.out.println(maxProfit(arr2)); // Expected: 0
-		System.out.println(maxProfit(arr3)); // Expected: 4
-		System.out.println(maxProfit(arr4)); // Expected: 2
-		System.out.println(maxProfit(arr5)); // Expected: 0
-		System.out.println(maxProfit(arr6)); // Expected: 0
-		System.out.println(maxProfit(arr7)); // Expected: 4
+		System.out.println(_01_maxProfit(arr1)); // Expected: 5
+		System.out.println(_01_maxProfit(arr2)); // Expected: 0
+		System.out.println(_01_maxProfit(arr3)); // Expected: 4
+		System.out.println(_01_maxProfit(arr4)); // Expected: 2
+		System.out.println(_01_maxProfit(arr5)); // Expected: 0
+		System.out.println(_01_maxProfit(arr6)); // Expected: 0
+		System.out.println(_01_maxProfit(arr7)); // Expected: 4
+
+		System.out.println("-----------------------------");
+
+		System.out.println(_01_maxProfit(arr1)); // Expected: 5
+		System.out.println(_01_maxProfit(arr2)); // Expected: 0
+		System.out.println(_01_maxProfit(arr3)); // Expected: 4
+		System.out.println(_01_maxProfit(arr4)); // Expected: 2
+		System.out.println(_01_maxProfit(arr5)); // Expected: 0
+		System.out.println(_01_maxProfit(arr6)); // Expected: 0
+		System.out.println(_01_maxProfit(arr7)); // Expected: 4
 	}
 
-	public static int maxProfit(int[] prices) {
+	public static int _01_maxProfit(int[] prices) {
 		// Time Complexity: O(n)
 		// Space Complexity: O(1)
 
@@ -35,6 +45,24 @@ public class _009_BestTimeToBuySellStocks {
 			maxProfit = Math.max(maxProfit, currentPrice - buyAt);
 			if (buyAt > currentPrice) {
 				buyAt = currentPrice;
+			}
+		}
+		return maxProfit;
+	}
+
+	public static int _02_maxProfit(int[] prices) {
+		// Time Complexity: O(n)
+		// Space Complexity: O(1)
+
+		if (prices.length == 0)
+			return 0;
+		int maxProfit = 0;
+		int left = 0;
+		for (int right = 1; right < prices.length; right++) {
+			if (prices[right] > prices[left]) {
+				maxProfit = Math.max(maxProfit, prices[right] - prices[left]);
+			} else {
+				left = right;
 			}
 		}
 		return maxProfit;
