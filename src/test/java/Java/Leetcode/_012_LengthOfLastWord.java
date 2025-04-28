d6c020607ef26787c82979198805da94da16532e
avdhutssh <avdhut.ssh@gmail.com>
2025-04-28 10:13:58 +0530

    _02_lengthOfLastWord_Optimized

--- src/test/java/Java/Leetcode/_012_LengthOfLastWord.java
+++ src/test/java/Java/Leetcode/_012_LengthOfLastWord.java
@@ -11,6 +11,16 @@ public class _012_LengthOfLastWord {
 		System.out.println(_01_lengthOfLastWord_Optimized(" ")); // 0
 		System.out.println(_01_lengthOfLastWord_Optimized("Today is a nice day ")); // 3
 		System.out.println(_01_lengthOfLastWord_Optimized("day")); // 3
+		
+		System.out.println("---------------------------------");
+		
+		System.out.println(_02_lengthOfLastWord_Optimized("Hello World")); // 5
+		System.out.println(_02_lengthOfLastWord_Optimized("   fly me   to   the moon  ")); // 4
+		System.out.println(_02_lengthOfLastWord_Optimized("luffy is still joyboy")); // 6
+		System.out.println(_02_lengthOfLastWord_Optimized("a")); // 1
+		System.out.println(_02_lengthOfLastWord_Optimized(" ")); // 0
+		System.out.println(_02_lengthOfLastWord_Optimized("Today is a nice day ")); // 3
+		System.out.println(_02_lengthOfLastWord_Optimized("day")); // 3
 	}
 
 	public static int _01_lengthOfLastWord_Optimized(String s) {
@@ -27,5 +37,21 @@ public class _012_LengthOfLastWord {
 		}
 
 		return right - left;
+	}
+
+	public static int _02_lengthOfLastWord_Optimized(String s) {
+		// Time Complexity: O(n)
+		// Space Complexity: O(1)
+
+		int count = 0;
+		for (int i = s.length() - 1; i >= 0; i--) {
+			if (s.charAt(i) != ' ') {
+				count++;
+			} else if (count > 0) {
+				return count;
+			}
+		}
+
+		return count;
 	}
 }
