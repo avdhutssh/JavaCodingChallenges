61e466f6967b74d056d8c8b36390e355cdd4b3fa
avdhutssh <avdhut.ssh@gmail.com>
2025-04-29 14:50:38 +0530

    FirstMissingPositive

---
+++ src/test/java/Java/Leetcode/_020_FirstMissingPositive.java
@@ -0,0 +1,35 @@
+package Java.Leetcode;
+
+public class _020_FirstMissingPositive {
+
+	public static void main(String[] args) {
+		System.out.println(firstMissingPositive(new int[] { 1, 2, 0 })); // Expected: 3
+		System.out.println(firstMissingPositive(new int[] { 3, 4, -1, 1 })); // Expected: 2
+		System.out.println(firstMissingPositive(new int[] { 7, 8, 9, 11, 12 })); // Expected: 1
+		System.out.println(firstMissingPositive(new int[] { 1, 2, 3, 4, 5 })); // Expected: 6
+		System.out.println(firstMissingPositive(new int[] {})); // Expected: 1
+		System.out.println(firstMissingPositive(new int[] { -1, -2, -3 })); // Expected: 1
+		System.out.println(firstMissingPositive(new int[] { 1, 1, 1 })); // Expected: 2
+	}
+
+	public static int firstMissingPositive(int[] nums) {
+		for (int i = 0; i < nums.length; i++) {
+			if (nums[i] <= 0) {
+				nums[i] = nums.length + 1;
+			}
+		}
+
+		for (int i = 0; i < nums.length; i++) {
+			int index = Math.abs(nums[i]) - 1;
+			if (index < nums.length && nums[index] > 0) {
+				nums[index] = -nums[index];
+			}
+		}
+		for (int i = 0; i < nums.length; i++) {
+			if (nums[i] > 0) {
+				return i + 1;
+			}
+		}
+		return nums.length + 1;
+	}
+}
