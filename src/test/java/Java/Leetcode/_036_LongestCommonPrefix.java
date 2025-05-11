bfd7cd0002d4ccefccc4664c4b4c31b6190ea105
avdhutssh <avdhut.ssh@gmail.com>
2025-05-11 13:36:13 +0530

    _02_longestCommonPrefix_Optimized

--- src/test/java/Java/Leetcode/_036_LongestCommonPrefix.java
+++ src/test/java/Java/Leetcode/_036_LongestCommonPrefix.java
@@ -15,12 +15,40 @@ public class _036_LongestCommonPrefix {
 		System.out.println(_01_longestCommonPrefix_BFA(strs3));
 		System.out.println(_01_longestCommonPrefix_BFA(strs4));
 		System.out.println(_01_longestCommonPrefix_BFA(strs5));
+
+		System.out.println("------------------------------");
+
+		System.out.println(_02_longestCommonPrefix_Optimized(strs1));
+		System.out.println(_02_longestCommonPrefix_Optimized(strs2));
+		System.out.println(_02_longestCommonPrefix_Optimized(strs3));
+		System.out.println(_02_longestCommonPrefix_Optimized(strs4));
+		System.out.println(_02_longestCommonPrefix_Optimized(strs5));
+	}
+
+	private static String _02_longestCommonPrefix_Optimized(String[] strs) {
+		// Time Complexity: O(n*m)
+		// Space Complexity: O(1)
+
+		if (strs == null || strs.length == 0) {
+			return "";
+		}
+		String prefix = strs[0];
+		for (int i = 0; i < strs.length; i++) {
+
+			while (strs[i].indexOf(prefix) != 0) {
+				prefix = prefix.substring(0, prefix.length() - 1);
+				if (prefix.isEmpty()) {
+					return "";
+				}
+			}
+		}
+		return prefix;
 	}
 
 	public static String _01_longestCommonPrefix_BFA(String[] strs) {
 		// Time Complexity: O(n*m)
 		// Space Complexity: O(1)
-		
+
 		if (strs == null || strs.length == 0)
 			return "";
 		for (int i = 0; i < strs[0].length(); i++) {
