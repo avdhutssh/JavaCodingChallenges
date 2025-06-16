0b3b24e9f58ffa795ea6561aa0257c2243bf7ca8
avdhutssh <avdhut.ssh@gmail.com>
2025-06-16 17:01:37 +0530

    ExerciseJune -> _03_compareArrayAndFindCommon

--- src/test/java/practice/ExerciseJune.java
+++ src/test/java/practice/ExerciseJune.java
@@ -1,5 +1,6 @@
 package practice;
 
+import java.util.HashSet;
 import java.util.stream.IntStream;
 
 public class ExerciseJune {
@@ -10,18 +11,28 @@ public class ExerciseJune {
 
 		System.out.println("-----------------------------------------");
 
-		int[] maxDiffArr = { 1, 4, 8, 15, 17,27 };
-		System.out.println(_02_MaxDiffBetweenAdjacentElementsInArr(maxDiffArr)); // 7
+		int[] maxDiffArr = { 1, 4, 8, 15, 17, 27 };
+		System.out.println(_02_MaxDiffBetweenAdjacentElementsInArr(maxDiffArr)); // 10
 
+		System.out.println("-----------------------------------------");
+
+		int[] a = { 1, 4, 5, 7 };
+		int[] b = { 6, 4, 3, 7 };
+		_03_compareArrayAndFindCommon(a, b); // 4 7
+
+		System.out.println("\n-----------------------------------------");
 	}
 
 	private static int _01_multiply(int num1, int num2) {
 		// Time complexity: O(num2)
 		// Space complexity: O(1)
+
 		return IntStream.generate(() -> num1).limit(num2).sum();
 	}
 
 	private static int _02_MaxDiffBetweenAdjacentElementsInArr(int[] arr) {
+		// Time complexity: O(n)
+		// Space complexity: O(1)
 
 		int maxDiff = Integer.MIN_VALUE;
 		for (int i = 0; i < arr.length - 1; i++) {
@@ -29,6 +40,22 @@ public class ExerciseJune {
 		}
 
 		return maxDiff;
+	}
+
+	private static void _03_compareArrayAndFindCommon(int[] a, int[] b) {
+		// Time complexity: O(n)
+		// Space complexity: O(n)
+
+		HashSet<Integer> hs = new HashSet<Integer>();
+		for (int num : a) {
+			hs.add(num);
+		}
+		for (int num : b) {
+			if (!hs.add(num)) {
+				System.out.print(num + " ");
+			}
+		}
+
 	}
 
 }
