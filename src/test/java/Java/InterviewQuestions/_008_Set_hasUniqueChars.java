e8ccb7f5e9063c64d97a15a936f39659f3ecab6e
avdhutssh <avdhut.ssh@gmail.com>
2025-06-17 08:35:59 +0530

    hasUniqueChars

--- src/test/java/Java/InterviewQuestions/_008_Set_hasUniqueChars.java
+++ src/test/java/Java/InterviewQuestions/_008_Set_hasUniqueChars.java
@@ -23,6 +23,12 @@ public class _008_Set_hasUniqueChars {
 		System.out.println(_02_hasUnique(str3));
 		System.out.println(_02_hasUnique(str4));
 		System.out.println(_02_hasUnique(str5));
+		System.out.println("--------------------------");
+		System.out.println(_03_hasUniqueOptimized(str1));
+		System.out.println(_03_hasUniqueOptimized(str2));
+		System.out.println(_03_hasUniqueOptimized(str3));
+		System.out.println(_03_hasUniqueOptimized(str4));
+		System.out.println(_03_hasUniqueOptimized(str5));
 	}
 
 	public static boolean _01_hasUnique(String str) {
@@ -44,5 +50,23 @@ public class _008_Set_hasUniqueChars {
 		// Space complexity: O(n)
 		Set<Character> charSet = str.chars().mapToObj(c -> (char) c).collect(Collectors.toSet());
 		return charSet.size() == str.length();
+	}
+
+	private static boolean _03_hasUniqueOptimized(String str) {
+		// Time complexity: O(n)
+		// Space complexity: O(1)
+		if (str.length() == 0)
+			return true;
+		if (str.length() > 128)
+			return false;
+
+		boolean[] seen = new boolean[128];
+		for (int i = 0; i < str.length(); i++) {
+			if (seen[str.charAt(i)]) {
+				return false;
+			}
+			seen[str.charAt(i)] = true;
+		}
+		return true;
 	}
 }
